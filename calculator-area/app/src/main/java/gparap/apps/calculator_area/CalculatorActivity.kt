59460d59e6a6ebca91866a8392f6cfaec8532c0c
gparap <gparap@protonmail.com>
2021-02-12 14:02:39 +0200

    Initial commit

---
+++ calculator-area/app/src/main/java/gparap/apps/calculator_area/CalculatorActivity.kt
@@ -0,0 +1,11 @@
+package gparap.apps.calculator_area
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalculatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
---
+++ calculator-area/app/src/test/java/gparap/apps/calculator_area/CalculatorUnitTest.kt
@@ -0,0 +1,13 @@
+package gparap.apps.calculator_area
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class CalculatorUnitTest {
+}
