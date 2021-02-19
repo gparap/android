f2cbab5662d179296a1e9a6f230087786e509a5e
gparap <gparap@protonmail.com>
2021-02-19 22:00:58 +0200

    convert from binary to text and vice versa

--- converter-binary/app/src/main/java/gparap/apps/converter_binary/Converter.kt
+++ converter-binary/app/src/main/java/gparap/apps/converter_binary/Converter.kt
@@ -10,14 +10,31 @@ class Converter {
      *  and converts them to binary using their byte value
      */
     fun convertTextToBinary(textString: String) : String {
-        val stringBuffer: StringBuffer = StringBuffer()
+        val stringBuffer = StringBuffer()
 
         val charArray: CharArray = textString.toCharArray()
         for (char: Char in charArray) {
-            val byte = char.toByte()
-            val binary = byte.toString(2)
-            stringBuffer.append(binary) //.append(" ") TODO: parse result
+            val byte: Byte = char.toByte()
+            val binary: String = byte.toString(2)
+            stringBuffer.append(String.format("%08d", binary.toInt()))  //restores missing zeros in
+        }                                                               // the front from conversion
+        return stringBuffer.toString()
+    }
+
+    /**
+     * Makes substrings from the text string in steps of 8
+     *  and converts them to char using their byte value
+     */
+    fun convertBinaryToText(binaryString: String): String {
+        val stringBuffer = StringBuffer()
+
+        val range = binaryString.length-1
+        for (i in 0..range step 8) {
+            val byte = Integer.parseInt(binaryString.substring(i, i+8), 2)//convert to byte
+            val char: Char = byte.toChar()                                      //convert to char
+            stringBuffer.append(char)
         }
+
         return stringBuffer.toString()
     }
 }
--- converter-binary/app/src/main/java/gparap/apps/converter_binary/MainActivity.kt
+++ converter-binary/app/src/main/java/gparap/apps/converter_binary/MainActivity.kt
@@ -5,14 +5,16 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 
+/**
+ * Created by gparap on 2021-02-19.
+ */
 class MainActivity : AppCompatActivity() {
-    lateinit var editTextResult :EditText
-    lateinit var buttonConvertToBinary: Button
-    lateinit var buttonConvertToText: Button
-    lateinit var buttonClear:Button
+    private lateinit var editTextResult :EditText
+    private lateinit var buttonConvertToBinary: Button
+    private lateinit var buttonConvertToText: Button
+    private lateinit var buttonClear:Button
     private val converter: Converter = Converter()
 
-    @ExperimentalStdlibApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,16 +25,17 @@ class MainActivity : AppCompatActivity() {
         buttonConvertToText= findViewById(R.id.buttonConvertToText)
         buttonClear= findViewById(R.id.buttonClear)
 
-        //add listeners
-        buttonClear.setOnClickListener { clear() }
+        //clear text
+        buttonClear.setOnClickListener { editTextResult.text.clear() }
+
+        //convert from binary to text
         buttonConvertToBinary.setOnClickListener {
             editTextResult.setText(converter.convertTextToBinary(editTextResult.text.toString()))
-
         }
-    }
 
-    // Clears text
-    private fun clear() {
-        editTextResult.text.clear()
+        //convert from text to binary
+        buttonConvertToText.setOnClickListener {
+            editTextResult.setText(converter.convertBinaryToText(editTextResult.text.toString()))
+        }
     }
 }
