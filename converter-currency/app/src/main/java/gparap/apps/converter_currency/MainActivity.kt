020fcea8896e6516c0f736278f5f153a578c21dc
gparap <gparap@protonmail.com>
2021-02-20 15:15:21 +0200

    basic layout

--- converter-currency/app/src/main/java/gparap/apps/converter_currency/MainActivity.kt
+++ converter-currency/app/src/main/java/gparap/apps/converter_currency/MainActivity.kt
@@ -3,6 +3,9 @@ package gparap.apps.converter_currency
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Created by gparap on 2021-02-20.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
