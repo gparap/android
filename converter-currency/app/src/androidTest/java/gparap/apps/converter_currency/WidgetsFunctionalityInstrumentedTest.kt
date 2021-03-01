92787a1270c837e197283517cb2202baa1d84202
gparap <gparap@protonmail.com>
2021-03-01 14:49:13 +0200

    display full currency names

---
+++ converter-currency/app/src/androidTest/java/gparap/apps/converter_currency/WidgetsFunctionalityInstrumentedTest.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2021 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gparap.apps.converter_currency
+
+import android.content.Context
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * Created by gparap on 2021-03-01.
+ */
+class WidgetsFunctionalityInstrumentedTest {
+    @get:Rule
+    val rule = ActivityScenarioRule(ConverterActivity::class.java)
+    private var context: Context? = null
+    val usDollarIndex = 1
+    @Before
+    fun setUp() {
+        //get base context
+        rule.scenario.onActivity { activity -> context = activity.baseContext }
+
+        //launch activity
+        ActivityScenario.launch(ConverterActivity::class.java)
+    }
+
+    @Test
+    fun displayCurrencyName_FromCurrencySpinner() {
+        onView(withId(R.id.spinnerFromCurrency)).perform(click())
+        onData(`is`(context?.resources?.getStringArray(R.array.currencyCodes)?.get(usDollarIndex))).perform(click())
+        onView(withId(R.id.textViewLabelFromCurrency)).check(matches(withText(R.string.string_USD)))
+    }
+
+    @Test
+    fun displayCurrencyName_ToCurrencySpinner() {
+        onView(withId(R.id.spinnerToCurrency)).perform(click())
+        onData(`is`(context?.resources?.getStringArray(R.array.currencyCodes)?.get(usDollarIndex))).perform(click())
+        onView(withId(R.id.textViewLabelToCurrency)).check(matches(withText(R.string.string_USD)))
+    }
+}
--- converter-currency/app/src/main/java/gparap/apps/converter_currency/ConverterActivity.kt
+++ converter-currency/app/src/main/java/gparap/apps/converter_currency/ConverterActivity.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package gparap.apps.converter_currency
 
 import androidx.appcompat.app.AppCompatActivity
@@ -16,18 +31,21 @@ class ConverterActivity : AppCompatActivity(), OnItemSelectedListener {
     lateinit var labelToCurrency: TextView
     lateinit var buttonConvert: Button
     lateinit var result: TextView
+    lateinit var currencies: HashMap<String, String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_converter)
+        initCurrencies()
         getWidgets()
         initSpinners()
         buttonConvert.setOnClickListener { convert() }
     }
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        labelFromCurrency.text = spinnerFromCurrency.selectedItem.toString()
-        labelToCurrency.text = spinnerToCurrency.selectedItem.toString()
+        //display full currency name
+        labelFromCurrency.text = currencies[spinnerFromCurrency.selectedItem.toString()]
+        labelToCurrency.text = currencies[spinnerToCurrency.selectedItem.toString()]
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -62,5 +80,18 @@ class ConverterActivity : AppCompatActivity(), OnItemSelectedListener {
     private fun convert() {
         //pass test
         result.text = "pass test"
+    }
+
+    /**
+     * Creates a collection of currencies
+     */
+    private fun initCurrencies() {
+        currencies = HashMap()
+        val count = 33
+        val codes = resources.getStringArray(R.array.currencyCodes)
+        val names = resources.getStringArray(R.array.currencyNames)
+        for (i in 0 until count) {
+            currencies[codes[i].toString()] = names[i].toString()
+        }
     }
 }
