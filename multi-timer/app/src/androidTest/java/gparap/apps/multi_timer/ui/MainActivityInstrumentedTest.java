c902104f4132652e9cb481539d6ee30ec1252fba
gparap <gparap@protonmail.com>
2021-03-09 13:47:18 +0200

    create custom toolbar

---
+++ multi-timer/app/src/androidTest/java/gparap/apps/multi_timer/ui/MainActivityInstrumentedTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gparap.apps.multi_timer.ui;
+
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import gparap.apps.multi_timer.R;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by gparap on 2021-03-09.
+ */
+public class MainActivityInstrumentedTest {
+
+    @Before
+    public void setUp() {
+        ActivityScenario.launch(MainActivity.class);
+    }
+
+    @Test
+    public void Toolbar_IsDisplayed() {
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
+    }
+}
