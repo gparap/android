f2bee51367a80f133a2519d888e0137e8fcc11a5
gparap <gparap@protonmail.com>
2021-03-12 13:54:41 +0200

    change app name

---
+++ multiplex-clock/app/src/androidTest/java/gparap/apps/multiplex_clock/MainActivityInstrumentedTest.java
@@ -0,0 +1,51 @@
+package gparap.apps.multiplex_clock;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import gparap.apps.multiplex_clock.ui.MainActivity;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+public class MainActivityInstrumentedTest {
+
+    @Before
+    public void setUp() {
+        ActivityScenario.launch(MainActivity.class);
+    }
+
+    @Test
+    public void Toolbar_IsDisplayed() {
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void AlarmClockFragment_IsDisplayed() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getContext());
+        onView(withText(R.string.item_alarm_clock)).perform(click());
+        onView(withId(R.id.alarmClockFragment)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void ChronometerFragment_IsDisplayed() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getContext());
+        onView(withText(R.string.item_chronometer)).perform(click());
+        onView(withId(R.id.chronometerFragment)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void CountdownTimerFragment_IsDisplayed() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getContext());
+        onView(withText(R.string.item_countdown_timer)).perform(click());
+        onView(withId(R.id.countdownTimerFragment)).check(matches(isDisplayed()));
+    }
+}
---
+++ multiplex-clock/app/src/main/java/gparap/apps/multiplex_clock/ui/AlarmClockFragment.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2021 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gparap.apps.multiplex_clock.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import gparap.apps.multiplex_clock.R;
+
+public class AlarmClockFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public AlarmClockFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AlarmClockFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AlarmClockFragment newInstance(String param1, String param2) {
+        AlarmClockFragment fragment = new AlarmClockFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_alarm_clock, container, false);
+    }
+}
