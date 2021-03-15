a7b7e15e778f2498575aa8089cc327457e6c6621
gparap <gparap@protonmail.com>
2021-03-15 16:40:47 +0200

    create layout for chronometer fragment

--- multiplex-clock/app/src/androidTest/java/gparap/apps/multiplex_clock/ui/MainActivityInstrumentedTest.java
+++ multiplex-clock/app/src/androidTest/java/gparap/apps/multiplex_clock/ui/MainActivityInstrumentedTest.java
@@ -1,11 +1,28 @@
-package gparap.apps.multiplex_clock;
+/*
+ * Copyright 2021 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gparap.apps.multiplex_clock.ui;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import gparap.apps.multiplex_clock.R;
 import gparap.apps.multiplex_clock.ui.MainActivity;
 
 import static androidx.test.espresso.Espresso.onView;
@@ -25,13 +42,13 @@ public class MainActivityInstrumentedTest {
 
     @Test
     public void Toolbar_IsDisplayed() {
-        onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
+        onView(ViewMatchers.withId(R.id.toolbar)).check(matches(isDisplayed()));
     }
 
     @Test
     public void ChronometerFragment_IsDisplayed() {
         openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getContext());
-        onView(withText(R.string.item_chronometer)).perform(click());
+        onView(withText(R.string.menu_item_chronometer)).perform(click());
         onView(withId(R.id.chronometerFragment)).check(matches(isDisplayed()));
     }
 
--- multiplex-clock/app/src/main/java/gparap/apps/multiplex_clock/ui/ChronometerFragment.java
+++ multiplex-clock/app/src/main/java/gparap/apps/multiplex_clock/ui/ChronometerFragment.java
@@ -22,55 +22,80 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 import gparap.apps.multiplex_clock.R;
 
 public class ChronometerFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Chronometer chronometer;
+    private Button buttonStart;
+    private Button buttonStop;
+    private Button buttonReset;
 
     public ChronometerFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChronometerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChronometerFragment newInstance(String param1, String param2) {
+    public static ChronometerFragment newInstance() {
         ChronometerFragment fragment = new ChronometerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chronometer, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chronometer, container, false);
+        getFragmentWidgets(view);
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        addOnClickListenersToFragmentWidgets();
+    }
+
+    private void startTimer() {
+        System.out.println("Chronometer started");
+    }
+
+    private void stopTimer() {
+        System.out.println("Chronometer stopped");
+    }
+
+    private void resetTimer() {
+        System.out.println("Chronometer reset");
+    }
+
+    private void addOnClickListenersToFragmentWidgets() {
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTimer();
+            }
+        });
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopTimer();
+            }
+        });
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+            }
+        });
+    }
+
+    private void getFragmentWidgets(View view) {
+        chronometer = view.findViewById(R.id.chronometer);
+        buttonStart = view.findViewById(R.id.buttonStart);
+        buttonStop = view.findViewById(R.id.buttonStop);
+        buttonReset = view.findViewById(R.id.buttonReset);
     }
 }
