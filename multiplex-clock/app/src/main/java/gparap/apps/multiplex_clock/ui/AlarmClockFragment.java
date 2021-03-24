ef7c7dbf96306b7ba46738d4064508aaf1ac8133
gparap <gparap@protonmail.com>
2021-03-24 20:12:16 +0200

    create a real clock simulation

--- multiplex-clock/app/src/main/java/gparap/apps/multiplex_clock/ui/AlarmClockFragment.java
+++ multiplex-clock/app/src/main/java/gparap/apps/multiplex_clock/ui/AlarmClockFragment.java
@@ -19,56 +19,95 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.text.DateFormat;
 
 public class AlarmClockFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView textViewTimer;
+    DateFormat dateFormat;  //helper to format system time to default locale
+    TimePicker timePicker;
+    Button buttonSetAlarm;
+    TextView textViewAlarm;
+    boolean isAlarmSet;     //helper to handle cases where the alarm is not set correctly
 
     public AlarmClockFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AlarmClockFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AlarmClockFragment newInstance(String param1, String param2) {
+    public static AlarmClockFragment newInstance() {
         AlarmClockFragment fragment = new AlarmClockFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alarm_clock, container, false);
+        View view = inflater.inflate(R.layout.fragment_alarm_clock, container, false);
+        getFragmentWidgets(view);
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        displayCurrentTime();
+        buttonSetAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: setAlarm
+            }
+        });
+    }
+
+    private void getFragmentWidgets(View view) {
+        textViewTimer = view.findViewById(R.id.textViewTimeNow);
+        timePicker = view.findViewById(R.id.timePicker);
+        buttonSetAlarm = view.findViewById(R.id.buttonSetAlarm);
+        textViewAlarm = view.findViewById(R.id.textViewAlarm);
+        isAlarmSet = false;
+
+        //format system time to default locale
+        dateFormat = DateFormat.getTimeInstance();
+    }
+
+    //simulate a real clock
+    private void displayCurrentTime() {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+
+                //keep running thread to simulate a real clock
+                while (!isInterrupted()) {
+                    try {
+                        //1 second intervals
+                        sleep(1000);
+
+                        //update current time
+                        new Handler(Looper.getMainLooper()).post(new Runnable() {
+                            @Override
+                            public void run() {
+                                textViewTimer.setText(dateFormat.format(System.currentTimeMillis()));
+                            }
+                        });
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        thread.start();
     }
 }
