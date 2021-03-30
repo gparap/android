a7b3a49e18c294837a175ae7d917f89eee9816f5
gparap <gparap@protonmail.com>
2021-03-30 15:16:25 +0200

    Update project

--- weather/app/src/main/java/gparap/apps/weather/MainActivity.java
+++ weather/app/src/main/java/gparap/apps/weather/MainActivity.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020-present gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package gparap.apps.weather;
 
 import android.annotation.SuppressLint;
@@ -29,9 +44,6 @@ import java.util.Random;
 
 import static android.view.View.VISIBLE;
 
-/**
- * Created by gparap on 2020-11-20.
- */
 @SuppressWarnings("Convert2Lambda")
 @SuppressLint("NonConstantResourceId")
 public class MainActivity extends AppCompatActivity {
