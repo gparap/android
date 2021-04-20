27786da9cd7a7b8e3ed6e23a51655368e44e2824
gparap <gparap@protonmail.com>
2021-04-20 16:47:34 +0200

    Initial commit

---
+++ password/app/src/androidTest/java/gparap/apps/password/BottomNavigationInstrumentedTest.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2021 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gparap.apps.password
+
+import androidx.appcompat.widget.Toolbar
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import org.junit.Before
+import org.junit.Test
+
+
+class BottomNavigationInstrumentedTest {
+
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun navigateToGeneratorFragment() {
+        //goto any other fragment first
+        onView(withId(R.id.navigation_manager)).perform(click())
+
+        onView(withId(R.id.navigation_generator)).perform(click())
+        onView(isAssignableFrom(Toolbar::class.java))
+            .check(matches(hasDescendant(withText(R.string.title_generator))))
+    }
+
+    @Test
+    fun navigateToEvaluatorFragment() {
+        onView(withId(R.id.navigation_evaluator)).perform(click())
+        onView(isAssignableFrom(Toolbar::class.java))
+            .check(matches(hasDescendant(withText(R.string.title_evaluator))))
+    }
+
+    @Test
+    fun navigateToManagerFragment() {
+        onView(withId(R.id.navigation_manager)).perform(click())
+        onView(isAssignableFrom(Toolbar::class.java))
+            .check(matches(hasDescendant(withText(R.string.title_manager))))
+    }
+}
