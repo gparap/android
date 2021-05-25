5c80d6b60fc4f569161b4672c0621d2967cc3ea3
gparap <gparap@protonmail.com>
2021-05-25 15:45:22 +0200

    change button to float action button in evaluator layout

--- password/app/src/main/java/gparap/apps/password/ui/evaluator/EvaluatorFragment.kt
+++ password/app/src/main/java/gparap/apps/password/ui/evaluator/EvaluatorFragment.kt
@@ -21,11 +21,11 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import gparap.apps.password.R
 import gparap.apps.password.utils.DatabaseUtils
 
@@ -94,7 +94,7 @@ class EvaluatorFragment : Fragment() {
         })
 
         //save password to database
-        val buttonSave = rootView.findViewById<Button>(R.id.buttonSaveEvaluatedPassword)
+        val buttonSave = rootView.findViewById<FloatingActionButton>(R.id.buttonSaveEvaluatedPassword)
         buttonSave.setOnClickListener {
             DatabaseUtils.getInstance()?.savePassword(
                 password.text.toString(),
