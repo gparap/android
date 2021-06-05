ad9a8697800a196c63f3b64eb50892eda683c110
gparap <gparap@protonmail.com>
2021-06-05 23:39:37 +0200

    refactor todo model class

--- todo-list/app/src/main/java/gparap/apps/todo_list/model/ToDoModel.java
+++ todo-list/app/src/main/java/gparap/apps/todo_list/model/ToDoModel.java
@@ -1,20 +1,35 @@
+/*
+ * Copyright 2021 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package gparap.apps.todo_list.model;
 
-/**
- * Model of a To_Do.
- * Created by gparap on 2020-10-15.
- */
-public class ToDoModel {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.text.DateFormat;
+
+public class ToDoModel implements Parcelable {
     private long id;
     private boolean isDone;
-    private String todo,
-                   time,
-                   date;
+    private String todo;
+    private Long deadlineTimeStamp;
+    private final Long creationTimeStamp;
 
     public long getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
@@ -22,11 +37,9 @@ public class ToDoModel {
     public boolean isDone() {
         return isDone;
     }
-
     public void setDone(boolean done) {
         isDone = done;
     }
-
     public void setDone(int done) {
         isDone = done == 1;
     }
@@ -34,27 +47,68 @@ public class ToDoModel {
     public String getTodo() {
         return todo;
     }
-
     public void setTodo(String todo) {
         this.todo = todo;
     }
 
-    public String getTime() {
-        return time;
+    public String getDeadlineTimeStampFormatted() {
+        //autoformat the to-do dealine timestamp to the default locale
+        return DateFormat.getDateTimeInstance().format(deadlineTimeStamp);
     }
 
-    public void setTime(String time) {
-        this.time = time;
+    public void setDeadlineTimeStamp(Long deadlineTimeStamp) {
+        this.deadlineTimeStamp = deadlineTimeStamp;
     }
 
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
+    public String getCreationTimeStampFormatted() {
+        //autoformat the to-do creation timestamp to the default locale
+        return DateFormat.getDateTimeInstance().format(creationTimeStamp);
     }
 
     public ToDoModel() {
+        deadlineTimeStamp = null;
+        creationTimeStamp = System.currentTimeMillis();
+    }
+
+    protected ToDoModel(Parcel in) {
+        id = in.readLong();
+        isDone = in.readByte() != 0;
+        todo = in.readString();
+        if (in.readByte() == 0) {
+            deadlineTimeStamp = null;
+        } else {
+            deadlineTimeStamp = in.readLong();
+        }
+        if (in.readByte() == 0) {
+            creationTimeStamp = null;
+        } else {
+            creationTimeStamp = in.readLong();
+        }
+    }
+
+    public static final Creator<ToDoModel> CREATOR = new Creator<ToDoModel>() {
+        @Override
+        public ToDoModel createFromParcel(Parcel in) {
+            return new ToDoModel(in);
+        }
+
+        @Override
+        public ToDoModel[] newArray(int size) {
+            return new ToDoModel[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(id);
+        dest.writeByte(isDone ? (byte) 1 : 0);
+        dest.writeString(todo);
+        dest.writeLong(deadlineTimeStamp);
+        dest.writeLong(creationTimeStamp);
     }
 }
