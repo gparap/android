b2d2050133c928ece2ebe58ef1f636c068f7a929
gparap <gparap@protonmail.com>
2021-06-14 14:35:00 +0200

    setup basic layout for editing an existing to-do

---
+++ todo-list/app/src/androidTest/java/gparap/apps/todo_list/ui/edit_todo/EditToDoFragmentInstrumentedTest.kt
@@ -0,0 +1,190 @@
+/*
+ * Copyright 2021 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gparap.apps.todo_list.ui.edit_todo
+
+import android.widget.DatePicker
+import android.widget.TimePicker
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.PickerActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import gparap.apps.todo_list.R
+import gparap.apps.todo_list.ui.add_todo.AddToDoFragment
+import org.hamcrest.Matchers
+import org.hamcrest.core.IsNot
+import org.junit.Before
+
+import org.junit.Test
+
+class EditToDoFragmentInstrumentedTest {
+
+    @Before
+    fun setUp() {
+        launchFragmentInContainer(null, R.style.Theme_MaterialComponents) {
+            EditToDoFragment()
+        }
+    }
+
+    @Test
+    fun onViewCreated_isFragmentVisible() {
+        Espresso.onView(ViewMatchers.withText(R.string.fragment_edit_todo))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun isVisible_editTextAddToDoUpdating() {
+        Espresso.onView(ViewMatchers.withId(R.id.editTextToDoUpdating))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun isVisible_fabUpdateToDoUpdating() {
+        Espresso.onView(ViewMatchers.withId(R.id.fabUpdateToDo))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun isVisible_labelToDoTimeUpdating() {
+        Espresso.onView(ViewMatchers.withId(R.id.labelDeadlineToDoUpdating))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun isVisible_textViewToDoDeadlineUpdating() {
+        Espresso.onView(ViewMatchers.withId(R.id.textViewToDoDeadlineUpdating))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun isInvisible_textViewToDoTimeSetUpdating() {
+        Espresso.onView(ViewMatchers.withId(R.id.textViewToDoTimeSetUpdating))
+            .check(ViewAssertions.matches(IsNot.not(ViewMatchers.isDisplayed())))
+    }
+
+    @Test
+    fun isVisible_buttonShowTimePickerDialogUpdating() {
+        Espresso.onView(ViewMatchers.withId(R.id.buttonShowTimePickerDialogUpdating))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun isInvisible_textViewToDoDateSetUpdating() {
+        Espresso.onView(ViewMatchers.withId(R.id.textViewToDoDateSetUpdating))
+            .check(ViewAssertions.matches(IsNot.not(ViewMatchers.isDisplayed())))
+    }
+
+    @Test
+    fun isVisible_buttonShowDatePickerDialogUpdating() {
+        Espresso.onView(ViewMatchers.withId(R.id.buttonShowDatePickerDialogUpdating))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun showDialog_timePickerUpdating() {
+        Espresso.onView(ViewMatchers.withId(R.id.buttonShowTimePickerDialogUpdating))
+            .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withClassName(Matchers.equalTo(TimePicker::class.java.name)))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun showDialog_datePickerUpdating() {
+        Espresso.onView(ViewMatchers.withId(R.id.buttonShowDatePickerDialogUpdating))
+            .perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withClassName(Matchers.equalTo(DatePicker::class.java.name)))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun textViewToDoTimeSetUpdating_displayTimePicked() {
+        val hours = 10
+        val minutes = 10
+        pickTime(hours, minutes)
+
+        //test
+        Espresso.onView(ViewMatchers.withId(R.id.textViewToDoTimeSetUpdating))
+            .check(ViewAssertions.matches(ViewMatchers.withText("$hours:$minutes")))
+    }
+
+    @Test
+    fun textViewToDoDateSetUpdating_displayDatePicked() {
+        val year = 2021
+        val month = 1
+        val day = 1
+        pickDate(year, month, day)
+
+        //test
+        Espresso.onView(ViewMatchers.withId(R.id.textViewToDoDateSetUpdating))
+            .check(ViewAssertions.matches(ViewMatchers.withText("$day/$month/$year")))
+    }
+
+    @Test
+    fun doesValuePersistOrientationChanges_ToDoTimeSetUpdating() {
+        val hours = 15
+        val minutes = 15
+        pickTime(hours, minutes)
+
+        //change screen orientation
+        val uiDevice: UiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        uiDevice.setOrientationLeft()
+        Thread.sleep(667)
+
+        Espresso.onView(ViewMatchers.withId(R.id.textViewToDoTimeSetUpdating))
+            .check(ViewAssertions.matches(ViewMatchers.withText("$hours:$minutes")))
+    }
+
+    @Test
+    fun doesValuePersistOrientationChanges_ToDoDateSetUpdating() {
+        val year = 2022
+        val month = 2
+        val day = 2
+        pickDate(year, month, day)
+
+        //change screen orientation
+        val uiDevice: UiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        uiDevice.setOrientationLeft()
+        Thread.sleep(667)
+
+        Espresso.onView(ViewMatchers.withId(R.id.textViewToDoDateSetUpdating))
+            .check(ViewAssertions.matches(ViewMatchers.withText("$day/$month/$year")))
+    }
+
+    private fun pickTime(hours: Int, minutes: Int) {
+        //show time picker dialog
+        Espresso.onView(ViewMatchers.withId(R.id.buttonShowTimePickerDialogUpdating))
+            .perform(ViewActions.click())
+
+        //pick time
+        Espresso.onView(ViewMatchers.withClassName(Matchers.equalTo(TimePicker::class.java.name)))
+            .perform(PickerActions.setTime(hours, minutes))
+        Espresso.onView(ViewMatchers.withId(android.R.id.button1)).perform(ViewActions.click())
+    }
+
+    private fun pickDate(year: Int, month: Int, day: Int) {
+        //show date picker dialog
+        Espresso.onView(ViewMatchers.withId(R.id.buttonShowDatePickerDialogUpdating))
+            .perform(ViewActions.click())
+
+        //pick date
+        Espresso.onView(ViewMatchers.withClassName(Matchers.equalTo(DatePicker::class.java.name)))
+            .perform(PickerActions.setDate(year, month, day))
+        Espresso.onView(ViewMatchers.withId(android.R.id.button1)).perform(ViewActions.click())
+    }
+}
---
+++ todo-list/app/src/main/java/gparap/apps/todo_list/ui/edit_todo/EditToDoFragment.kt
@@ -0,0 +1,25 @@
+package gparap.apps.todo_list.ui.edit_todo
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import gparap.apps.todo_list.R
+
+class EditToDoFragment : Fragment() {
+    private lateinit var viewModel: EditToDoViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_edit_todo, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(EditToDoViewModel::class.java)
+    }
+}
