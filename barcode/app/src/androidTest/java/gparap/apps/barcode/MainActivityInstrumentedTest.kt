70b7f8b68a9dd6bd017f22830bfcd7f99d867df2
gparap <gparap@protonmail.com>
2021-07-21 17:46:13 +0200

    create barcode generator

--- barcode/app/src/androidTest/java/gparap/apps/barcode/MainActivityInstrumentedTest.kt
+++ barcode/app/src/androidTest/java/gparap/apps/barcode/MainActivityInstrumentedTest.kt
@@ -15,18 +15,33 @@
  */
 package gparap.apps.barcode
 
+import android.graphics.Matrix
+import android.view.View
+import android.widget.ImageView
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.closeSoftKeyboard
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
 import androidx.test.espresso.matcher.ViewMatchers.*
+import org.hamcrest.core.IsNot.not
 import org.junit.Before
 import org.junit.Test
 
 class MainActivityInstrumentedTest {
+    private lateinit var activityScenario: ActivityScenario<MainActivity>
+    private lateinit var decorView: View
+
     @Before
     fun setUp() {
-        ActivityScenario.launch(MainActivity::class.java)
+        //launch activity
+        activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        //retrieve the top-level window decor view
+        activityScenario.onActivity {
+            decorView = it.window.decorView
+        }
     }
 
     @Test
@@ -44,5 +59,44 @@ class MainActivityInstrumentedTest {
     fun navigateToGeneratorFragment() {
         onView(withId(R.id.navigation_generator)).perform(click())
         onView(withId(R.id.action_bar)).check(matches(hasDescendant(withText(R.string.title_generator))))
+    }
+
+    @Test
+    fun generatorFragment_textToGenerateBarcodeIsEmpty_displayToast() {
+        //goto generator fragment
+        onView(withId(R.id.navigation_generator)).perform(click())
+
+        //make sure edit text is empty and attempt to generate code
+        onView(withId(R.id.editTextGenerateBarcode)).perform(clearText())
+        closeSoftKeyboard()
+        onView(withId(R.id.buttonGenerateBarcode)).perform(click())
+
+        onView(withText(R.string.toast_empty_text_generator))
+            .inRoot(withDecorView(not((decorView))))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun generatorFragment_generateBarcode() {
+        //goto generator fragment
+        onView(withId(R.id.navigation_generator)).perform(click())
+
+        //make sure the barcode's image matrix is the identity matrix
+        var identityMatrix: Matrix? = null
+        activityScenario.onActivity {
+            val imageView: ImageView = it.findViewById(R.id.imageViewGeneratedBarcode)
+            imageView.setImageBitmap(null)
+            identityMatrix = imageView.imageMatrix
+        }
+
+        //enter barcode
+        onView(withId(R.id.editTextGenerateBarcode)).perform(typeText("barcode"))
+        closeSoftKeyboard()
+        onView(withId(R.id.buttonGenerateBarcode)).perform(click())
+
+        activityScenario.onActivity {
+            val imageView: ImageView = it.findViewById(R.id.imageViewGeneratedBarcode)
+            assert(imageView.imageMatrix != identityMatrix)
+        }
     }
 }
