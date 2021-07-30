64ef2fe3546d6325c079a6047e49d979cc2c75a9
gparap <gparap@protonmail.com>
2021-07-30 14:26:34 +0200

    Initial commit

---
+++ painter/app/src/main/java/gparap/apps/painter/PainterActivity.kt
@@ -0,0 +1,11 @@
+package gparap.apps.painter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PainterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
