6335ff6fcccdf5130c0adf45398eed652b709c7b
gparap <gparap@protonmail.com>
2021-08-01 14:23:09 +0200

    refactor layout

--- painter/app/src/main/java/gparap/apps/painter/ui/CanvasView.kt
+++ painter/app/src/main/java/gparap/apps/painter/ui/CanvasView.kt
@@ -1,11 +1,12 @@
-package gparap.apps.painter
+package gparap.apps.painter.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import gparap.apps.painter.R
 
-class PainterView : AppCompatActivity() {
+class CanvasView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.view_painter)
+        setContentView(R.layout.canvas_area)
     }
 }
