a23f5a614daa421198bbb8c627a30e2a2d620b52
gparap <gparap@protonmail.com>
2021-08-02 13:54:57 +0200

    create basic functionality for drawing on canvas

--- painter/app/src/main/java/gparap/apps/painter/MainActivity.kt
+++ painter/app/src/main/java/gparap/apps/painter/MainActivity.kt
@@ -2,10 +2,18 @@ package gparap.apps.painter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createCanvasArea()
+    }
+
+    private fun createCanvasArea() {
+        val canvasLayout: ConstraintLayout = findViewById(R.id.main_canvas_area)
+        val canvasView = CanvasView(this, null)
+        canvasLayout.addView(canvasView)
     }
 }
