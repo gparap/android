cb6ea1972a94565036f57b19104e30c4c945efff
gparap <gparap@protonmail.com>
2021-09-11 16:45:54 +0200

    create app instrumented tests

--- news/app/src/androidTest/java/gparap/apps/news/MainActivityInstrumentedTest.kt
+++ news/app/src/androidTest/java/gparap/apps/news/MainActivityInstrumentedTest.kt
@@ -15,18 +15,100 @@
  */
 package gparap.apps.news
 
+import android.widget.ListView
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        Assert.assertEquals("gparap.apps.news", appContext.packageName)
+    private lateinit var activityScenario: ActivityScenario<MainActivity>
+
+    @Before
+    fun setUp() {
+        activityScenario = ActivityScenario.launch(MainActivity::class.java)
     }
+
+    @Test
+    fun isVisible_listViewStories() {
+        onView(withId(R.id.listViewStories)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun isVisible_buttonLoadMoreStories() {
+        onView(withId(R.id.buttonLoadMoreStories)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun onAppLoad_loadStories(){
+        var storiesCount = 0
+
+        //get stories count
+        activityScenario.onActivity { activityScenarioRule ->
+            val listView = activityScenarioRule.findViewById<ListView>(R.id.listViewStories)
+            storiesCount = listView.adapter?.count!!
+        }
+
+        assert(storiesCount > 0)
+    }
+
+    @Test
+    fun onButtonClick_loadMoreStories(){
+        var storiesCountBefore = 0
+        var storiesCountAfter = 0
+
+        //get stories count before loading more of them
+        activityScenario.onActivity { activityScenarioRule ->
+            val listView = activityScenarioRule.findViewById<ListView>(R.id.listViewStories)
+            storiesCountBefore = listView.adapter?.count!!
+        }
+
+        //load more stories (and wait a little to load)
+        onView(withId(R.id.buttonLoadMoreStories)).perform(click())
+        Thread.sleep(5000)
+
+        //get stories count after loading more of them
+        activityScenario.onActivity { activityScenarioRule ->
+            val listView = activityScenarioRule.findViewById<ListView>(R.id.listViewStories)
+            storiesCountAfter = listView.adapter?.count!!
+        }
+
+        assert(storiesCountAfter > storiesCountBefore)
+    }
+
+    @Test
+    fun onMenuOptionRefreshClick_removeStoriesAndLoadFromTheStart() {
+        var storiesCountRefresh = 0
+        var storiesCountLoadMore = 0
+
+        //load more stories (and wait a little to load)
+        onView(withId(R.id.buttonLoadMoreStories)).perform(click())
+
+        //get stories count after loading more of them
+        activityScenario.onActivity { activityScenarioRule ->
+            val listView = activityScenarioRule.findViewById<ListView>(R.id.listViewStories)
+            storiesCountLoadMore = listView.adapter?.count!!
+        }
+
+        //refresh stories
+        Espresso.openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().context)
+        onView(withText(R.string.refresh)).perform(click())
+
+        //get stories count after refreshing them
+        activityScenario.onActivity { activityScenarioRule ->
+            val listView = activityScenarioRule.findViewById<ListView>(R.id.listViewStories)
+            storiesCountRefresh = listView.adapter?.count!!
+        }
+
+        assert(storiesCountRefresh < storiesCountLoadMore)
+    }
+
 }
