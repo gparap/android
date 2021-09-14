5357f2849108197d1bc8d972ad6ca3a7e405ac26
gparap <gparap@protonmail.com>
2021-09-14 17:04:40 +0200

    create layout for color picker activity

---
+++ painter/app/src/main/java/gparap/apps/painter/color_picker/ColorPickerActivity.kt
@@ -0,0 +1,12 @@
+package gparap.apps.painter.color_picker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import gparap.apps.painter.R
+
+class ColorPickerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_color_picker)
+    }
+}
