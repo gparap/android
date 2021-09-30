70e1722ae26ed8ea02da247eed26dc02dfaead7c
gparap <gparap@protonmail.com>
2021-09-30 17:47:44 +0200

    save painting to device's public directory, "pictures"

--- painter/app/src/main/java/gparap/apps/painter/MainActivity.kt
+++ painter/app/src/main/java/gparap/apps/painter/MainActivity.kt
@@ -15,7 +15,10 @@
  */
 package gparap.apps.painter
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
@@ -26,14 +29,17 @@ import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import gparap.apps.painter.canvas.CanvasView
 import gparap.apps.painter.color_picker.ColorPickerDialog
 import gparap.apps.painter.color_picker.ColorPickerListener
 import gparap.apps.painter.utils.Utils
 
+@Suppress("PrivatePropertyName")
 class MainActivity : AppCompatActivity(), ColorPickerListener {
     private lateinit var canvasView: CanvasView
     private lateinit var dialog: ColorPickerDialog
+    private val REQUEST_CODE_WRITE_EXTERNAL_STORAGE = 111
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -99,13 +105,61 @@ class MainActivity : AppCompatActivity(), ColorPickerListener {
         //save painting to a .png file on the primary external filesystem
         val imageViewSave = findViewById<ImageView>(R.id.imageViewSave)
         imageViewSave.setOnClickListener {
-            Utils.writeDataToFile(
-                Utils.createNewFile(
-                    this.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!.absolutePath,
-                    Utils.generateFilenameSuffix("yyMMddHHmmss")
-                ), Utils.getByteArray(canvasView.bitmap)
-            )
+            //we should always request permissions if we encounter a legacy API
+            if (Build.VERSION.SDK_INT < 29) {
+                if (!isPermissionGranted()) {
+                    requestPermissions(
+                        arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                        REQUEST_CODE_WRITE_EXTERNAL_STORAGE
+                    )
+                }
+            } else {
+                savePainting()
+            }
         }
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_CODE_WRITE_EXTERNAL_STORAGE -> {
+                //if request is cancelled, the result arrays are empty
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                ) {
+                    //permission to save to device storage is granted
+                    savePainting()
+                }
+                return
+            }
+        }
+    }
+
+    //check if permission to write storage is granted (for API < 29)
+    private fun isPermissionGranted(): Boolean {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    //save painting to device's public directory, "pictures"
+    private fun savePainting() {
+        Utils.writeDataToFile(
+            Utils.createNewFile(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                    .toString(),
+                Utils.generateFilenameSuffix("yyMMddHHmmss")
+            ), Utils.getByteArray(canvasView.bitmap)
+        )
     }
 
     //pick a color as the current color
