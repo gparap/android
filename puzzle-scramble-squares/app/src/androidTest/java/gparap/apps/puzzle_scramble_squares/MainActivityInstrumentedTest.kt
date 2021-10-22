5cb82079c0d2ce4ba2d9e0896ee80e70fd67195d
gparap <gparap@protonmail.com>
2021-10-22 15:40:24 +0200

    Pick an image from file storage and display it on puzzle activity

---
+++ puzzle-scramble-squares/app/src/androidTest/java/gparap/apps/puzzle_scramble_squares/MainActivityInstrumentedTest.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2021 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gparap.apps.puzzle_scramble_squares
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class MainActivityInstrumentedTest {
+
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun useAppContext() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("gparap.apps.puzzle_scramble_squares", appContext.packageName)
+    }
+
+    @Test
+    fun isVisible_button_image_picker() {
+        onView(withId(R.id.button_image_picker)).check(matches(isDisplayed()))
+    }
+}
