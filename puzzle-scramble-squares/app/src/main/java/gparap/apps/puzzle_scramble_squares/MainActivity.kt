661aaf4ce4bfd216e5599db92dadea365c11aed2
gparap <gparap@protonmail.com>
2021-10-22 12:53:38 +0200

    Initial commit

---
+++ puzzle-scramble-squares/app/src/main/java/gparap/apps/puzzle_scramble_squares/MainActivity.kt
@@ -0,0 +1,11 @@
+package gparap.apps.puzzle_scramble_squares
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
