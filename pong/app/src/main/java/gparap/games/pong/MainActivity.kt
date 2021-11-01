4df9ef635813bd910d6f0d4e23d1e812042d252c
gparap <gparap@protonmail.com>
2021-11-01 16:31:12 +0200

    Create activity for pong (orientation & fullscreen)

--- pong/app/src/main/java/gparap/games/pong/MainActivity.kt
+++ pong/app/src/main/java/gparap/games/pong/MainActivity.kt
@@ -1,11 +1,19 @@
 package gparap.games.pong
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //start playing pong
+        val buttonPlay = findViewById<Button>(R.id.buttonPlay)
+        buttonPlay.setOnClickListener {
+            startActivity(Intent(this, PongActivity::class.java))
+        }
     }
 }
