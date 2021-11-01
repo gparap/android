31c422d222a6c87d7da0ef2d1e7197342eb1c0c9
gparap <gparap@protonmail.com>
2021-11-01 15:29:47 +0200

    Initial commit

---
+++ pong/app/src/test/java/gparap/games/pong/ExampleUnitTest.kt
@@ -0,0 +1,19 @@
+package gparap.games.pong
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        val a = 2
+        val b = 2
+        assertEquals(4, a + b)
+    }
+}
