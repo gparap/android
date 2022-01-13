143bd936b15afd56ab174139063b7285458471f0
gparap <gparap@protonmail.com>
2022-01-13 15:35:18 +0200

    Create a recycler view with adapter to handle the movies list

--- movies/app/src/main/java/gparap/apps/movies/MainActivity.kt
+++ movies/app/src/main/java/gparap/apps/movies/MainActivity.kt
@@ -2,10 +2,19 @@ package gparap.apps.movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import gparap.apps.movies.adapters.MovieAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //create a recyclerView with adapter to display the movies list
+        val recyclerViewMovies = findViewById<RecyclerView>(R.id.recycler_view_movies)
+        recyclerViewMovies.layoutManager = GridLayoutManager(this, 2)
+        val moviesAdapter = MovieAdapter()
+        recyclerViewMovies.adapter = moviesAdapter
     }
 }
