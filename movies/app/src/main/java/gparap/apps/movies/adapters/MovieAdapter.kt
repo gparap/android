166ed024b3600c7238e3339745b2c2ffd13bac5d
gparap <gparap@protonmail.com>
2022-01-13 15:35:18 +0200

    Create a recycler view with adapter to handle the movies list

---
+++ movies/app/src/main/java/gparap/apps/movies/adapters/MovieAdapter.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2022 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gparap.apps.movies.adapters
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import gparap.apps.movies.R
+import gparap.apps.movies.model.MovieModel
+
+class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MoviesViewHolder>() {
+    var movies = ArrayList<MovieModel>()
+        @SuppressLint("NotifyDataSetChanged")
+        set(value) {
+            field = value
+
+            //the data set has changed
+            Handler(Looper.getMainLooper()).post {
+                notifyDataSetChanged()
+            }
+        }
+
+    //only use for placeholder image
+    private var context: Context? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MoviesViewHolder {
+        //only use for placeholder image
+        context = parent.context
+
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.cardview_details, parent, false)
+        return MoviesViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MoviesViewHolder, position: Int) {
+        //placeholder
+        holder.image?.setImageDrawable(context?.resources?.getDrawable(R.drawable.placeholder))
+    }
+
+    override fun getItemCount(): Int {
+        return movies.size
+    }
+
+    class MoviesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val image: ImageView? = itemView.findViewById(R.id.image_view_movie)
+    }
+}
