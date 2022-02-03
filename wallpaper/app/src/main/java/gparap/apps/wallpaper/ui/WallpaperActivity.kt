94a4e152d671fac37a90a056d0f278ad8ba7581c
gparap <gparap@protonmail.com>
2022-02-03 15:54:19 +0200

    Create a floating action menu in wallpaper activity

--- wallpaper/app/src/main/java/gparap/apps/wallpaper/ui/WallpaperActivity.kt
+++ wallpaper/app/src/main/java/gparap/apps/wallpaper/ui/WallpaperActivity.kt
@@ -15,13 +15,50 @@
  */
 package gparap.apps.wallpaper.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import gparap.apps.wallpaper.R
 
-class WallpaperActivity : AppCompatActivity() {
+class WallpaperActivity : AppCompatActivity(), PopupMenu.OnMenuItemClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wallpaper)
+
+        //create a popup menu
+        val fabMenu = findViewById<FloatingActionButton>(R.id.fab_menu)
+        fabMenu.setOnClickListener {
+            val popup = PopupMenu(this, it)
+            val inflater: MenuInflater = popup.menuInflater
+            inflater.inflate(R.menu.fab_menu, popup.menu)
+            popup.show()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                popup.setForceShowIcon(true)
+            }
+            popup.setOnMenuItemClickListener(this)
+        }
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.fab_menu_item_set_wallpaper -> {
+                Toast.makeText(this, "placeholder1", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.fab_menu_item_save_wallpaper -> {
+                Toast.makeText(this, "placeholder2", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.fab_menu_item_wallpaper_details -> {
+                Toast.makeText(this, "placeholder3", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+        return false
     }
 }
