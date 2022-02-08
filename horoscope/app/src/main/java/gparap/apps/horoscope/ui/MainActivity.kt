a2605b930f84a7bb0dc1c9cbb11ced5e4ba6f7e1
gparap <gparap@protonmail.com>
2022-02-08 18:09:53 +0200

    Implement a ViewModel and refactor code

--- horoscope/app/src/main/java/gparap/apps/horoscope/ui/MainActivity.kt
+++ horoscope/app/src/main/java/gparap/apps/horoscope/ui/MainActivity.kt
@@ -21,176 +21,77 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import gparap.apps.horoscope.R
 import gparap.apps.horoscope.adapters.SpinnerAdapter
 import gparap.apps.horoscope.data.HoroscopeModel
-import gparap.apps.horoscope.services.AztroService
-import gparap.apps.horoscope.services.RetrofitClient
+import gparap.apps.horoscope.utils.Utils
+import gparap.apps.horoscope.viewmodels.MainActivityViewModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
-    private lateinit var service: AztroService
-    private lateinit var requestedDay: String
-    private var response: Call<HoroscopeModel>? = null
-    private lateinit var zodiacSign: String
+    private lateinit var viewModel: MainActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //create the ViewModel and setup what we will need for this activity
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        viewModel.setRequestedDay(resources.getString(R.string.text_today))
+        viewModel.setZodiacSign(this.resources.getString(R.string.text_prompt_select_spinner))
+        viewModel.createWebService()
 
         //setup spinner with adapter
         val spinner = findViewById<Spinner>(R.id.spinner_zodiac_signs)
         spinner.adapter = SpinnerAdapter.create(this, R.array.zodiac_signs)
         spinner.onItemSelectedListener = this
         spinner.setSelection(0)
-
-        //set the default option (today) for horoscope
-        requestedDay = resources.getString(R.string.text_today)
-
-        //set the prompt value as the default for the zodiac sign
-        zodiacSign = this.resources.getString(R.string.text_prompt_select_spinner)
-
-        //create web service
-        service = RetrofitClient.build().create(AztroService::class.java)
     }
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
         //do nothing if nothing is selected
         if (parent?.getItemAtPosition(position).toString()
             == this.resources.getString(R.string.text_prompt_select_spinner)
-        ) {
-            return
-        }
+        ) return
 
         //set the selected zodiac sign
-        zodiacSign = parent?.getItemAtPosition(position).toString()
+        viewModel.setZodiacSign(parent?.getItemAtPosition(position).toString())
 
-        //create a web service request based on selected day and on selected zodiac sign
-        createWebService(requestedDay, zodiacSign)
+        //get the horoscope
+        createWebServiceRequest(viewModel.getRequestedDay(), viewModel.getZodiacSign())
+        requestHoroscopeFromWebService()
 
-        //call web service and get horoscope
-        callWebService()
-
-        //hide prompt text and show the horoscope view
+        //hide prompt text
         findViewById<TextView>(R.id.text_view_prompt_select_sign).apply {
             visibility = View.GONE
         }
+
+        //show the horoscope view
         findViewById<ScrollView>(R.id.scroll_view_horoscope).apply {
             visibility = View.VISIBLE
         }
 
-        //set the zodiac sign and its date range
-        when (parent?.getItemAtPosition(position).toString()) {
-            //Aries
-            this.resources.getString(R.string.text_zodiac_Aries) -> {
-                findViewById<ImageView>(R.id.image_view_zodiac_sign).apply {
-                    this.setImageResource(R.drawable.aries)
-                }
-                findViewById<TextView>(R.id.text_view_date_range).apply {
-                    this.setText(R.string.date_range_Aries)
-                }
-            }
-            //Taurus
-            this.resources.getString(R.string.text_zodiac_Taurus) -> {
-                findViewById<ImageView>(R.id.image_view_zodiac_sign).apply {
-                    this.setImageResource(R.drawable.taurus)
-                }
-                findViewById<TextView>(R.id.text_view_date_range).apply {
-                    this.setText(R.string.date_range_Taurus)
-                }
-            }
-            //Gemini
-            this.resources.getString(R.string.text_zodiac_Gemini) -> {
-                findViewById<ImageView>(R.id.image_view_zodiac_sign).apply {
-                    this.setImageResource(R.drawable.gemini)
-                }
-                findViewById<TextView>(R.id.text_view_date_range).apply {
-                    this.setText(R.string.date_range_Gemini)
-                }
-            }
-            //Cancer
-            this.resources.getString(R.string.text_zodiac_Cancer) -> {
-                findViewById<ImageView>(R.id.image_view_zodiac_sign).apply {
-                    this.setImageResource(R.drawable.cancer)
-                }
-                findViewById<TextView>(R.id.text_view_date_range).apply {
-                    this.setText(R.string.date_range_Cancer)
-                }
-            }
-            //Leo
-            this.resources.getString(R.string.text_zodiac_Leo) -> {
-                findViewById<ImageView>(R.id.image_view_zodiac_sign).apply {
-                    this.setImageResource(R.drawable.leo)
-                }
-                findViewById<TextView>(R.id.text_view_date_range).apply {
-                    this.setText(R.string.date_range_Leo)
-                }
-            }
-            //Virgo
-            this.resources.getString(R.string.text_zodiac_Virgo) -> {
-                findViewById<ImageView>(R.id.image_view_zodiac_sign).apply {
-                    this.setImageResource(R.drawable.virgo)
-                }
-                findViewById<TextView>(R.id.text_view_date_range).apply {
-                    this.setText(R.string.date_range_Virgo)
-                }
-            }
-            //Libra
-            this.resources.getString(R.string.text_zodiac_Libra) -> {
-                findViewById<ImageView>(R.id.image_view_zodiac_sign).apply {
-                    this.setImageResource(R.drawable.libra)
-                }
-                findViewById<TextView>(R.id.text_view_date_range).apply {
-                    this.setText(R.string.date_range_Libra)
-                }
-            }
-            //Scorpio
-            this.resources.getString(R.string.text_zodiac_Scorpio) -> {
-                findViewById<ImageView>(R.id.image_view_zodiac_sign).apply {
-                    this.setImageResource(R.drawable.scorpio)
-                }
-                findViewById<TextView>(R.id.text_view_date_range).apply {
-                    this.setText(R.string.date_range_Scorpio)
-                }
-            }
-            //Sagittarius
-            this.resources.getString(R.string.text_zodiac_Sagittarius) -> {
-                findViewById<ImageView>(R.id.image_view_zodiac_sign).apply {
-                    this.setImageResource(R.drawable.sagittarius)
-                }
-                findViewById<TextView>(R.id.text_view_date_range).apply {
-                    this.setText(R.string.date_range_Sagittarius)
-                }
-            }
-            //Capricorn
-            this.resources.getString(R.string.text_zodiac_Capricorn) -> {
-                findViewById<ImageView>(R.id.image_view_zodiac_sign).apply {
-                    this.setImageResource(R.drawable.capricorn)
-                }
-                findViewById<TextView>(R.id.text_view_date_range).apply {
-                    this.setText(R.string.date_range_Capricorn)
-                }
-            }
-            //Aquarius
-            this.resources.getString(R.string.text_zodiac_Aquarius) -> {
-                findViewById<ImageView>(R.id.image_view_zodiac_sign).apply {
-                    this.setImageResource(R.drawable.aquarius)
-                }
-                findViewById<TextView>(R.id.text_view_date_range).apply {
-                    this.setText(R.string.date_range_Aquarius)
-                }
-            }
-            //Pisces
-            this.resources.getString(R.string.text_zodiac_Pisces) -> {
-                findViewById<ImageView>(R.id.image_view_zodiac_sign).apply {
-                    this.setImageResource(R.drawable.pisces)
-                }
-                findViewById<TextView>(R.id.text_view_date_range).apply {
-                    this.setText(R.string.date_range_Pisces)
-                }
-            }
+        //set the zodiac sign drawable
+        findViewById<ImageView>(R.id.image_view_zodiac_sign).apply {
+            this.setImageResource(
+                Utils.getZodiacSignDrawableResourceIdByName(
+                    this@MainActivity,
+                    parent?.getItemAtPosition(position).toString()
+                )
+            )
+        }
+
+        //set the zodiac sign date range
+        findViewById<TextView>(R.id.text_view_date_range).apply {
+            this.setText(
+                Utils.getZodiacSignDateRangeStringByName(
+                    this@MainActivity,
+                    parent?.getItemAtPosition(position).toString()
+                )
+            )
         }
     }
 
@@ -201,75 +102,67 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         return super.onCreateOptionsMenu(menu)
     }
 
-    //change the requested day and get horoscope
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        //get the requested day
-        when (item.itemId) {
-            R.id.menu_item_today -> {
-                requestedDay = resources.getString(R.string.text_today)
-            }
-            R.id.menu_item_tomorrow -> {
-                requestedDay = resources.getString(R.string.text_tomorrow)
-            }
-            R.id.menu_item_yesterday -> {
-                requestedDay = resources.getString(R.string.text_yesterday)
-            }
+        //get the requested day for the horoscope
+        viewModel.setRequestedDay(Utils.getRequestedDayById(this, item.itemId))
+
+        //get the horoscope only if a zodiac sign is actually selected
+        if (viewModel.getZodiacSign() != this.resources.getString(R.string.text_prompt_select_spinner)) {
+            createWebServiceRequest(viewModel.getRequestedDay(), viewModel.getZodiacSign())
+            requestHoroscopeFromWebService()
         }
-        //get horoscope only if a zodiac sign is actually selected
-        if (zodiacSign != this.resources.getString(R.string.text_prompt_select_spinner)) {
-            createWebService(requestedDay, zodiacSign)
-            callWebService()
-        }
+
         return super.onOptionsItemSelected(item)
     }
 
-    //create a web service request based on selected day and on selected zodiac sign
-    private fun createWebService(day: String, sign: String) {
+    //create a web service request based on selected day and zodiac sign
+    private fun createWebServiceRequest(day: String?, sign: String?) {
         when (day) {
             //get today's horoscope
             resources.getString(R.string.text_today) ->
-                response = service.getHoroscopeForToday(sign)
+                viewModel.setApiResponse(viewModel.getWebService()?.getHoroscopeForToday(sign)
+                )
 
             //get tomorrow's horoscope
             resources.getString(R.string.text_tomorrow) ->
-                response = service.getHoroscopeForTomorrow(sign)
+                viewModel.setApiResponse(viewModel.getWebService()?.getHoroscopeForTomorrow(sign))
 
             //get yesterdays's horoscope
             resources.getString(R.string.text_yesterday) ->
-                response = service.getHoroscopeForYesterday(sign)
+                viewModel.setApiResponse(viewModel.getWebService()?.getHoroscopeForYesterday(sign))
         }
     }
 
     //call web service and get horoscope
-    private fun callWebService() {
-        response?.enqueue(object : Callback<HoroscopeModel> {
+    private fun requestHoroscopeFromWebService() {
+        viewModel.getApiResponse()?.enqueue(object : Callback<HoroscopeModel> {
             override fun onResponse(
                 call: Call<HoroscopeModel>,
                 response: Response<HoroscopeModel>,
             ) {
-                //get horoscope object
-                val horoscopeObj = response.body() as HoroscopeModel
-
-                //display horoscope details
-                val date = findViewById<TextView>(R.id.text_view_date)
-                date.text = horoscopeObj.date
-                val horoscope = findViewById<TextView>(R.id.text_view_horoscope)
-                horoscope.text = horoscopeObj.horoscope
-                val number = findViewById<TextView>(R.id.text_view_lucky_number)
-                number.text = horoscopeObj.luckyNumber
-                val time = findViewById<TextView>(R.id.text_view_lucky_time)
-                time.text = horoscopeObj.luckyTime
-                val color = findViewById<TextView>(R.id.text_view_lucky_color)
-                color.text = horoscopeObj.luckyColor
-                val pair = findViewById<TextView>(R.id.text_view_pair_sign)
-                pair.text = horoscopeObj.pair
+                displayHoroscope(response.body() as HoroscopeModel)
             }
 
             override fun onFailure(call: Call<HoroscopeModel>, t: Throwable) {
-                //inform the user that something went wrong
                 Toast.makeText(this@MainActivity, t.localizedMessage, Toast.LENGTH_LONG)
                     .show()
             }
         })
+    }
+
+    //display the full horoscope details
+    private fun displayHoroscope(model: HoroscopeModel) {
+        val date = findViewById<TextView>(R.id.text_view_date)
+        date.text = model.date
+        val horoscope = findViewById<TextView>(R.id.text_view_horoscope)
+        horoscope.text = model.horoscope
+        val number = findViewById<TextView>(R.id.text_view_lucky_number)
+        number.text = model.luckyNumber
+        val time = findViewById<TextView>(R.id.text_view_lucky_time)
+        time.text = model.luckyTime
+        val color = findViewById<TextView>(R.id.text_view_lucky_color)
+        color.text = model.luckyColor
+        val pair = findViewById<TextView>(R.id.text_view_pair_sign)
+        pair.text = model.pair
     }
 }
