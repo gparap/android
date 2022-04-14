78e6b70a8650012c06615185498a5bfd5442afb8
gparap <gparap@protonmail.com>
2022-04-14 18:24:49 +0200

    Create functionality for loading an interstitial ad

--- movies/app/src/main/java/gparap/apps/movies/MainActivity.kt
+++ movies/app/src/main/java/gparap/apps/movies/MainActivity.kt
@@ -15,21 +15,35 @@
  */
 package gparap.apps.movies
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import gparap.apps.movies.adapters.MovieAdapter
 import gparap.apps.movies.model.MovieModel
 import gparap.apps.movies.model.MovieResponseModel
 import gparap.apps.movies.services.MovieService
 import gparap.apps.movies.services.RetrofitClient
 import gparap.apps.movies.utils.AppConstants
+import gparap.apps.movies.utils.AppConstants.APP_OPENED_TIMES_BY_USER
+import gparap.apps.movies.utils.AppConstants.INTERSTITIAL_AD_UNIT_ID
+import gparap.apps.movies.utils.AppConstants.INTERSTITIAL_LOAD_FACTOR
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    @Suppress("PrivatePropertyName")
+    private val TAG = "Mobile Ads SDK"
+    private var interstitialAd: InterstitialAd? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -50,7 +64,7 @@ class MainActivity : AppCompatActivity() {
         response?.enqueue(object : Callback<MovieResponseModel?> {
             override fun onResponse(
                 call: Call<MovieResponseModel?>,
-                response: Response<MovieResponseModel?>
+                response: Response<MovieResponseModel?>,
             ) {
                 //get all movies
                 val movies: List<MovieModel>? = response.body()?.movies
@@ -63,5 +77,43 @@ class MainActivity : AppCompatActivity() {
                 println(t.message.toString())
             }
         })
+
+        //initialize the Mobile Ads SDK
+        MobileAds.initialize(this) { }
+
+        //get the times that user opened the app
+        val sharedPref = getSharedPreferences(APP_OPENED_TIMES_BY_USER, Context.MODE_PRIVATE)
+        val timesUserOpenedTheApp = sharedPref.getInt(APP_OPENED_TIMES_BY_USER, 1)
+
+        //load an interstitial ad every X-th time the user opened the app !!! NOT the 1st time !!!
+        if (timesUserOpenedTheApp % INTERSTITIAL_LOAD_FACTOR == 0) {
+            //load ad
+            InterstitialAd.load(this, INTERSTITIAL_AD_UNIT_ID,
+                AdRequest.Builder().build(),
+                object : InterstitialAdLoadCallback() {
+                    override fun onAdFailedToLoad(adError: LoadAdError) {
+                        Log.d(TAG, adError.message)
+                        interstitialAd = null
+                    }
+
+                    override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                        Log.d(TAG, "Ad was loaded.")
+                        this@MainActivity.interstitialAd = interstitialAd
+
+                        //show the ad
+                        if (this@MainActivity.interstitialAd != null) {
+                            this@MainActivity.interstitialAd?.show(this@MainActivity)
+
+                        } else {
+                            Log.d(TAG, "The interstitial ad wasn't ready yet.")
+                        }
+                    }
+                })
+        }
+
+        //update the times that user opened the app
+        val editor = sharedPref.edit()
+        editor?.putInt(APP_OPENED_TIMES_BY_USER, timesUserOpenedTheApp + 1)
+        editor?.apply()
     }
 }
--- movies/app/src/main/java/gparap/apps/movies/utils/AppConstants.kt
+++ movies/app/src/main/java/gparap/apps/movies/utils/AppConstants.kt
@@ -2,4 +2,7 @@ package gparap.apps.movies.utils
 
 object AppConstants {
     const val BASE_URL = "http://gparap.com"
+    const val INTERSTITIAL_AD_UNIT_ID = "ca-app-pub-4227032191105066/3546490002"
+    const val APP_OPENED_TIMES_BY_USER = "app_opened_times"
+    const val INTERSTITIAL_LOAD_FACTOR = 3
 }
