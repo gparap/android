14ec8b6865783aa2c4921d9fd86056f9c5465a8b
gparap <gparap@protonmail.com>
2022-04-18 10:55:32 +0200

    Use the SSL link for the API and fix app name

--- movies/app/src/main/java/gparap/apps/movies/utils/AppConstants.kt
+++ movies/app/src/main/java/gparap/apps/movies/utils/AppConstants.kt
@@ -1,7 +1,7 @@
 package gparap.apps.movies.utils
 
 object AppConstants {
-    const val BASE_URL = "http://gparap.com"
+    const val BASE_URL = "https://www.gparap.com"
     const val API_KEY = "MY_API_KEY"
     const val INTERSTITIAL_AD_UNIT_ID = "ca-app-pub-4227032191105066/3546490002"
     const val APP_OPENED_TIMES_BY_USER = "app_opened_times"
