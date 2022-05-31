c7bad304c1640f9aa11f8b752e13a46a647e205a
gparap <gparap@protonmail.com>
2022-05-31 14:58:55 +0200

    Create basic layout

---
+++ launcher/app/src/androidTest/java/gparap/apps/launcher/MainActivityInstrumentedTest.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2022 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gparap.apps.launcher
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Before
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityInstrumentedTest {
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    @SmallTest
+    fun useAppContext() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("gparap.apps.launcher", appContext.packageName)
+    }
+
+    @Test
+    @SmallTest
+    fun isVisible_frameLayout() {
+        onView(withId(R.id.frame_layout_apps).check(matches(isDisplayed()))
+    }
+
+    @Test
+    @SmallTest
+    fun isVisible_gridView() {
+        onView(withId(R.id.grid_view_apps)).check(matches(isDisplayed()))
+    }
+}
