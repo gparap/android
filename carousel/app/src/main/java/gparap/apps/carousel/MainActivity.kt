6dfe666b17b466f1b0d0cf0fd4380c7e0597e655
gparap <gparap@protonmail.com>
2022-08-11 17:44:29 +0200

    Create carousel

--- carousel/app/src/main/java/gparap/apps/carousel/MainActivity.kt
+++ carousel/app/src/main/java/gparap/apps/carousel/MainActivity.kt
@@ -1,11 +1,43 @@
+/*
+ * Copyright (c) 2022 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package gparap.apps.carousel
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import gparap.apps.carousel.adapters.ImageAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //create the images list
+        val images = listOf(
+            R.drawable.image_01,
+            R.drawable.image_02,
+            R.drawable.image_03,
+            R.drawable.image_04,
+            R.drawable.image_05,
+            R.drawable.image_06,
+            R.drawable.image_07,
+        )
+
+        //create the carousel
+        val carousel = findViewById<ViewPager2>(R.id.carousel)
+        carousel.adapter = ImageAdapter(images)
     }
 }
---
+++ carousel/app/src/main/java/gparap/apps/carousel/adapters/ImageAdapter.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2022 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gparap.apps.carousel.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import gparap.apps.carousel.R
+
+class ImageAdapter(private val images: List<Int>) :
+    RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.carousel_item_view, parent, false)
+        return ImageViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.imageView.setImageResource(images[position])
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+
+    class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById<ImageView>(R.id.carouselImage)
+    }
+}
