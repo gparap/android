2f12717515171bc66e544aa610e8aee8fd163a89
gparap <gparap@protonmail.com>
2022-09-22 14:33:02 +0200

    Create a RecyclerView with adapter for blog posts

--- paidagogaki-gr/app/src/main/java/gparap/apps/paidagogaki_gr/MainActivity.kt
+++ paidagogaki-gr/app/src/main/java/gparap/apps/paidagogaki_gr/MainActivity.kt
@@ -17,10 +17,26 @@ package gparap.apps.paidagogaki_gr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import gparap.apps.paidagogaki_gr.adapters.PostAdapter
+import gparap.apps.paidagogaki_gr.data.PostModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //create a test list of posts
+        val posts = mutableListOf<PostModel>()
+        posts.add(PostModel(1, "1/1/2022", "1/1/2022", "", "test1"))
+        posts.add(PostModel(2, "2/1/2022", "2/1/2022", "", "test2"))
+        posts.add(PostModel(3, "3/1/2022", "3/1/2022", "", "test3"))
+        posts.add(PostModel(4, "4/1/2022", "4/1/2022", "", "test4"))
+
+        //create a RecyclerView with adapter for posts
+        val postsRecyclerView = findViewById<RecyclerView>(R.id.recycleViewMain)
+        postsRecyclerView.layoutManager = LinearLayoutManager(this)
+        postsRecyclerView.adapter = PostAdapter().apply { setPosts(posts) }
     }
 }
---
+++ paidagogaki-gr/app/src/main/java/gparap/apps/paidagogaki_gr/adapters/PostAdapter.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2022 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gparap.apps.paidagogaki_gr.adapters
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import gparap.apps.paidagogaki_gr.R
+import gparap.apps.paidagogaki_gr.data.PostModel
+
+/** Adapter & ViewHolder for Blog's posts */
+class PostAdapter : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
+    private lateinit var context: Context
+    private var posts: MutableList<PostModel> = mutableListOf()
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setPosts(posts: List<PostModel>) {
+        this.posts = posts.toMutableList()
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+        context = parent.context
+        return PostViewHolder(
+            LayoutInflater.from(context).inflate(
+                context.resources.getLayout(R.layout.cardview_post), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+        holder.image.setImageDrawable(AppCompatResources.getDrawable(
+            context, R.drawable.ic_android_48dp
+        ))
+        holder.title.text = posts[position].title
+    }
+
+    override fun getItemCount(): Int {
+        return posts.size
+    }
+
+    inner class PostViewHolder(itemView: View) : ViewHolder(itemView) {
+        val image: ImageView = itemView.findViewById(R.id.imageViewBrand)
+        val title: TextView = itemView.findViewById(R.id.textViewPostTitle)
+    }
+}
