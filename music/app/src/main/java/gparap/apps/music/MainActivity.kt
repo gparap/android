d2747cb645ce23004b2623ae6d0cbea470c137a9
gparap <gparap@protonmail.com>
2022-10-03 15:39:00 +0200

    Create basic layout

--- music/app/src/main/java/gparap/apps/music/MainActivity.kt
+++ music/app/src/main/java/gparap/apps/music/MainActivity.kt
@@ -1,11 +1,41 @@
+/*
+ * Copyright (c) 2022 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package gparap.apps.music
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import gparap.apps.music.adapters.SongsAdapter
+import gparap.apps.music.data.SongModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //create a test son list
+        val songs = ArrayList<SongModel>()
+        songs.add(SongModel("song 1", "00:00", "0 KB"))
+        songs.add(SongModel("song 2", "00:00", "0 KB"))
+        songs.add(SongModel("song 3", "00:00", "0 KB"))
+
+        //create recycler view for songs with adapter
+        val recyclerViewSongs = findViewById<RecyclerView>(R.id.recyclerViewSongs)
+        recyclerViewSongs.layoutManager = LinearLayoutManager(this)
+        recyclerViewSongs.adapter = SongsAdapter().apply { setSongs(songs) }
     }
 }
---
+++ music/app/src/main/java/gparap/apps/music/adapters/SongsAdapter.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2022 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gparap.apps.music.adapters
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import gparap.apps.music.R
+import gparap.apps.music.data.SongModel
+
+class SongsAdapter : RecyclerView.Adapter<SongsAdapter.SongsViewHolder>() {
+    private lateinit var context: Context
+    private var songs = ArrayList<SongModel>()
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setSongs(songs: ArrayList<SongModel>) {
+        this.songs = songs
+        notifyDataSetChanged()
+    }
+
+    class SongsViewHolder(itemView: View) : ViewHolder(itemView) {
+        val icon: ImageView = itemView.findViewById(R.id.imageViewSongIcon)
+        val title: TextView = itemView.findViewById(R.id.textViewSongTitle)
+        val duration: TextView = itemView.findViewById(R.id.textViewSongDuration)
+        val size: TextView = itemView.findViewById(R.id.textViewSongSize)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongsViewHolder {
+        //get parent context
+        context = parent.context
+
+        //create item view
+        return SongsViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(
+                    parent.context.resources.getLayout(R.layout.cardview_song), parent, false
+                )
+        )
+    }
+
+    override fun onBindViewHolder(holder: SongsViewHolder, position: Int) {
+        holder.icon.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_launcher_foreground))
+        holder.title.text = songs[position].title
+        holder.duration.text = songs[position].duration
+        holder.size.text = songs[position].size
+    }
+
+    override fun getItemCount(): Int {
+        return songs.size
+    }
+}
