3d194e10d233e0f47e135f80faf8d920f0b82976
gparap <gparap@protonmail.com>
2023-01-10 15:13:05 +0200

    Validate user registration

--- authentication/app/src/main/java/gparap/apps/authentication/RegisterActivity.kt
+++ authentication/app/src/main/java/gparap/apps/authentication/RegisterActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import gparap.apps.authentication.api.AuthenticationService
 import gparap.apps.authentication.api.RetrofitClient
 import gparap.apps.authentication.data.AuthenticationModel
+import gparap.apps.authentication.utils.Validator
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -32,19 +33,47 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
         //get user details
-        val username: EditText = findViewById<EditText>(R.id.editTextUsername)
-        val email = findViewById<EditText>(R.id.editTextEmail_register)
-        val password = findViewById<EditText>(R.id.editTextPassword_register)
-
-        //TODO: validate password confirmation
+        val editTextUsername: EditText = findViewById<EditText>(R.id.editTextUsername)
+        val username = editTextUsername.text.toString()
+        val editTextEmail = findViewById<EditText>(R.id.editTextEmail_register)
+        val email = editTextEmail.text.toString()
+        val editTextPassword = findViewById<EditText>(R.id.editTextPassword_register)
+        val password = editTextPassword.text.toString()
+        val editTextPasswordConfirm = findViewById<EditText>(R.id.editTextPasswordConfirm_register)
+        val passwordConfirm = editTextPasswordConfirm.text.toString()
 
         //create web service client
         val service = RetrofitClient().create()
 
-        //register user
+        //validate input & register user
         findViewById<Button>(R.id.buttonRegister).setOnClickListener {
+            //validate the user input
+            if (Validator.isEmpty(username) || Validator.isEmpty(email)
+                || Validator.isEmpty(password) || Validator.isEmpty(passwordConfirm)
+            ) {
+                println("Error: empty input")
+                return@setOnClickListener
+            }
+            if (!Validator.isEmailValid(email)) {
+                println("Error: invalid email")
+                return@setOnClickListener
+            }
+            if (!Validator.isPasswordLongEnough(password)) {
+                println("Error: Password too short")
+                return@setOnClickListener
+            }
+            if (!Validator.isPasswordTooLong(password)) {
+                println("Error: Password too long")
+                return@setOnClickListener
+            }
+            if (!Validator.doPasswordsMatch(password, passwordConfirm)) {
+                println("Error: Passwords do not match")
+                return@setOnClickListener
+            }
+
+            //register user
             service.create(AuthenticationService::class.java).createUserWithEmailAndPassword(
-                username.text.toString(), email.text.toString(), password.text.toString()
+                username, email, password
             )?.enqueue(object : Callback<AuthenticationModel?> {
                 override fun onResponse(
                     call: Call<AuthenticationModel?>,
--- authentication/app/src/main/java/gparap/apps/authentication/utils/AppConstants.kt
+++ authentication/app/src/main/java/gparap/apps/authentication/utils/AppConstants.kt
@@ -17,4 +17,6 @@ package gparap.apps.authentication.utils
  */
 object AppConstants {
     const val BASE_URL = "http://10.0.2.2/authentication/services/"
+    const val MIN_PASSWORD_LENGTH = 8
+    const val MAX_PASSWORD_LENGTH = 16
 }
