f7669b855d1820b3577e83f257e1c345ecc976fd
gparap <gparap@protonmail.com>
2023-04-04 13:24:17 +0300

    Setup the bottom navigation bar

--- recipes/app/src/main/java/gparap/apps/recipes/MainActivity.kt
+++ recipes/app/src/main/java/gparap/apps/recipes/MainActivity.kt
@@ -2,10 +2,21 @@ package gparap.apps.recipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //setup bottom navigation
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_nav_host) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.view_bottom_nav)
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
