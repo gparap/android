238b68b6dc3bf07adc6b7f832b241c0fd3987765
gparap <gparap@protonmail.com>
2023-10-06 13:52:14 +0300

    Open PDF files

--- pdf-reader/app/build.gradle.kts
+++ pdf-reader/app/build.gradle.kts
@@ -46,4 +46,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Android view for displaying PDFs
+    implementation("com.github.barteksc:android-pdf-viewer:3.2.0-beta.1")
 }
--- pdf-reader/app/src/main/java/gparap/apps/pdf_reader/MainActivity.kt
+++ pdf-reader/app/src/main/java/gparap/apps/pdf_reader/MainActivity.kt
@@ -1,11 +1,66 @@
+/*
+ * Copyright 2023 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package gparap.apps.pdf_reader
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.github.barteksc.pdfviewer.PDFView
 
 class MainActivity : AppCompatActivity() {
+    private var uri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //restore previously open PDF after orientation changes
+        if (savedInstanceState != null
+            && !(savedInstanceState.getString("pdf_uri").isNullOrEmpty())
+        ) {
+            uri = Uri.parse(savedInstanceState.getString("pdf_uri"))
+            findViewById<PDFView>(R.id.pdfView).apply {
+                this.fromUri(uri).load()
+            }
+        }
+
+        //select PDF
+        findViewById<Button>(R.id.pdfButton).setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "application/pdf"
+            startActivityForResult(intent, 999)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if (uri != null) {
+            outState.putString("pdf_uri", uri.toString())
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 999) {
+            uri = data?.data
+            findViewById<PDFView>(R.id.pdfView).apply {
+                this.fromUri(uri).load()
+            }
+        }
     }
 }
