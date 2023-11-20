0dbf839e859558abfe2228a66aa76cd7bfefce89
gparap <gparap@protonmail.com>
2023-11-20 15:15:24 +0200

    Create the book categories menu in OpenLibraryFragment

--- open-book-library/app/src/main/java/gparap/apps/open_book_library/ui/OpenLibraryFragment.kt
+++ open-book-library/app/src/main/java/gparap/apps/open_book_library/ui/OpenLibraryFragment.kt
@@ -1,33 +1,34 @@
+/*
+ * Copyright 2023 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package gparap.apps.open_book_library.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import gparap.apps.open_book_library.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [OpenLibraryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class OpenLibraryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -38,23 +39,28 @@ class OpenLibraryFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_open_library, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OpenLibraryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OpenLibraryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    @Deprecated("Deprecated in Java")
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_book_categories, menu)
     }
+
+    @Deprecated("Deprecated in Java")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_book_category_ancient_classics -> TODO("not implemented yet.")
+            R.id.menu_book_category_biography -> TODO("not implemented yet.")
+            R.id.menu_book_category_folklore -> TODO("not implemented yet.")
+            R.id.menu_book_category_history -> TODO("not implemented yet.")
+            R.id.menu_book_category_non_fiction -> TODO("not implemented yet.")
+            R.id.menu_book_category_novel -> TODO("not implemented yet.")
+            R.id.menu_book_category_poetry -> TODO("not implemented yet.")
+            R.id.menu_book_category_philosophy -> TODO("not implemented yet.")
+            R.id.menu_book_category_science_fiction -> TODO("not implemented yet.")
+            R.id.menu_book_category_short_stories -> TODO("not implemented yet.")
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
