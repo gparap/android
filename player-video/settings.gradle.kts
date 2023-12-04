5534f071993d73e790db33e7ccb2b9bf3d21d4b1
gparap <gparap@protonmail.com>
2023-12-04 13:24:10 +0200

    Initial commit (Video Player)

---
+++ player-video/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My Video Player Application"
+include(":app")
