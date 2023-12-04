8ded802359f220a7311bed6945c54359587a2c99
gparap <gparap@protonmail.com>
2023-12-04 14:15:34 +0200

    Create layout for the video item

---
+++ player-video/app/src/androidTest/java/gparap/apps/player_video/MainActivityInstrumentedTest.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2023 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gparap.apps.player_video
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityInstrumentedTest {
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(MainActivity::class.java)
+    }
+    @Test
+    fun isVisible_recyclerViewVideos() {
+        onView(withId(R.id.recycler_view_videos)).check(matches(isDisplayed()))
+    }
+}
