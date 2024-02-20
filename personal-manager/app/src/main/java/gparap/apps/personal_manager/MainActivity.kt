fec6f03bd67035670a7ab527dcc30fb762d426bc
gparap <gparap@protonmail.com>
2024-02-20 13:50:16 +0200

    Display objectives in the recycler view using a temporary list

--- personal-manager/app/src/main/java/gparap/apps/personal_manager/MainActivity.kt
+++ personal-manager/app/src/main/java/gparap/apps/personal_manager/MainActivity.kt
@@ -1,11 +1,49 @@
+/*
+ * Copyright 2024 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package gparap.apps.personal_manager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import gparap.apps.personal_manager.adapters.ObjectivesAdapter
+import gparap.apps.personal_manager.data.ObjectiveModel
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //create a test list of objectives
+        val testObjectives = ArrayList<ObjectiveModel>()
+        testObjectives.addAll(
+            listOf(
+                ObjectiveModel("objective 1", "desc 1", Date(2024, 31, 1), Date(2024, 31, 12)),
+                ObjectiveModel("objective 2", "desc 2", Date(2024, 31, 2), Date(2024, 31, 12)),
+                ObjectiveModel("objective 3", "desc 3", Date(2024, 31, 3), Date(2024, 31, 12)),
+                ObjectiveModel("objective 4", "desc 4", Date(2024, 31, 4), Date(2024, 31, 12)),
+                ObjectiveModel("objective 5", "desc 5", Date(2024, 31, 5), Date(2024, 31, 12))
+            )
+        )
+
+        //create recycler view with adapter
+        val adapterObjectives = ObjectivesAdapter().apply { objectives = testObjectives }
+        val recyclerViewObjectives = findViewById<RecyclerView>(R.id.recycler_view_objectives)
+        recyclerViewObjectives.layoutManager = LinearLayoutManager(this)
+        recyclerViewObjectives.adapter = adapterObjectives
     }
 }
---
+++ personal-manager/app/src/main/java/gparap/apps/personal_manager/adapters/ObjectivesAdapter.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2024 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package gparap.apps.personal_manager.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import gparap.apps.personal_manager.R
+import gparap.apps.personal_manager.data.ObjectiveModel
+import java.util.Date
+
+class ObjectivesAdapter : Adapter<ObjectivesAdapter.ObjectivesViewHolder>() {
+    var objectives = ArrayList<ObjectiveModel>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ObjectivesViewHolder {
+        //create the inflated item view
+        val context = parent.context
+        val view = LayoutInflater.from(context).inflate(R.layout.cardview_objective, parent, false)
+        return ObjectivesViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return objectives.size
+    }
+
+    override fun onBindViewHolder(holder: ObjectivesViewHolder, position: Int) {
+        //display the objective details
+        holder.title.text = objectives[position].title
+        holder.description.text = objectives[position].description
+        holder.dueDate.text = Date(objectives[position].dueDate.toString()).toString()
+        holder.insceptionDate.text = Date(objectives[position].inceptionDate.toString()).toString()
+    }
+
+    class ObjectivesViewHolder(itemView: View) : ViewHolder(itemView) {
+        val title = itemView.findViewById<TextView>(R.id.cardview_objective_title)
+        val description = itemView.findViewById<TextView>(R.id.cardview_objective_description)
+        val dueDate = itemView.findViewById<TextView>(R.id.cardview_objective_due_date)
+        val insceptionDate = itemView.findViewById<TextView>(R.id.cardview_objective_inception_date)
+    }
+}
