9b0db161a2854f5ce0b129e3598a393a3813bc1d
gparap <gparap@protonmail.com>
2024-02-26 10:10:26 +0200

    Create basic layout for UpdateObjective activity

--- personal-manager/app/src/androidTest/java/gparap/apps/personal_manager/ApplicationTestSuite.kt
+++ personal-manager/app/src/androidTest/java/gparap/apps/personal_manager/ApplicationTestSuite.kt
@@ -22,6 +22,7 @@ import org.junit.runners.Suite.SuiteClasses
 @RunWith(Suite::class)
 @SuiteClasses(
     MainActivityInstrumentedTest::class,
-    AddObjectiveActivityInstrumentedTest::class
+    AddObjectiveActivityInstrumentedTest::class,
+    UpdateObjectiveActivityInstrumentedTest::class
 )
 class ApplicationTestSuite
---
+++ personal-manager/app/src/main/java/gparap/apps/personal_manager/ui/UpdateObjectiveActivity.kt
@@ -0,0 +1,12 @@
+package gparap.apps.personal_manager.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import gparap.apps.personal_manager.R
+
+class UpdateObjectiveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_objective)
+    }
+}
