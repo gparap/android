2f254446dabdae4555cf3b73604d9dce6f0d5772
gparap <gparap@protonmail.com>
2024-04-09 14:21:18 +0300

    Encrypt & Decrypt input values based on the Triple DES algorithm

--- cipher-manager/app/src/main/java/gparap/apps/cipher_manager/utils/Algorithm.kt
+++ cipher-manager/app/src/main/java/gparap/apps/cipher_manager/utils/Algorithm.kt
@@ -3,7 +3,8 @@ package gparap.apps.cipher_manager.utils
 enum class Algorithm {
     AES,        //Advanced Encryption Standard
     ARC4,       //Rivest Cipher 4
-    RSA,        //Rivest–Shamir–Adleman
     Blowfish,
-    DES,        //Date Encryption Standard
+    DES,        //Data Encryption Standard
+    DESede,     //Triple Data Encryption Standard
+    RSA,        //Rivest–Shamir–Adleman
 }
