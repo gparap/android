50a04f6e5a3690352b3410bfa9f781c9922461a9
gparap <gparap@protonmail.com>
2024-09-10 12:35:36 +0300

    Create the basic layout for a grid of 2x4 cards

--- memory-matcher/app/build.gradle.kts
+++ memory-matcher/app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.gridlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
