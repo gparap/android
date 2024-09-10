1574fadc180a61b909ca9e3e3523dd4f4a484432
gparap <gparap@protonmail.com>
2024-09-10 12:35:36 +0300

    Create the basic layout for a grid of 2x4 cards

---
+++ memory-matcher/app/src/androidTest/java/gparap/apps/memory_matcher/MainActivityInstrumentedTest.kt
@@ -0,0 +1,36 @@
+package gparap.apps.memory_matcher
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityInstrumentedTest {
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun isVisible_grid() {
+        onView(withId(R.id.layout_memory_grid)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun areVisible_gridCards_2x4() {
+        onView(withId(R.id.grid_row_0_col_0)).check(matches(isDisplayed()))
+        onView(withId(R.id.grid_row_0_col_1)).check(matches(isDisplayed()))
+        onView(withId(R.id.grid_row_1_col_0)).check(matches(isDisplayed()))
+        onView(withId(R.id.grid_row_1_col_1)).check(matches(isDisplayed()))
+        onView(withId(R.id.grid_row_2_col_0)).check(matches(isDisplayed()))
+        onView(withId(R.id.grid_row_2_col_1)).check(matches(isDisplayed()))
+        onView(withId(R.id.grid_row_3_col_0)).check(matches(isDisplayed()))
+        onView(withId(R.id.grid_row_3_col_1)).check(matches(isDisplayed()))
+    }
+}
