2b65612d2f24a46d830bb44036755e7f93562d79
gparap <gparap@protonmail.com>
2024-09-24 11:58:04 +0300

    Create a manager to handle the state of the memory grid

--- memory-matcher/app/src/main/java/gparap/apps/memory_matcher/utils/AppConstants.kt
+++ memory-matcher/app/src/main/java/gparap/apps/memory_matcher/utils/AppConstants.kt
@@ -3,7 +3,8 @@ package gparap.apps.memory_matcher.utils
 object AppConstants {
     //state
     const val KEY_IS_GRID_FILLED    = "is_grid_filled"
-    const val KEY_GRID              = "grid"
+    const val KEY_GRID_SIZE         = "grid_size"
+    const val KEY_GRID_CARD_LIST    = "grid_card_list"
 
     //path
     const val PATH_PLANETS          = "planets"
