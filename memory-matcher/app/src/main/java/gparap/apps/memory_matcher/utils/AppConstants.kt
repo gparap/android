51d5ab4cca0de79d2c8b42a3b481687cf13e4cde
gparap <gparap@protonmail.com>
2024-09-30 11:41:52 +0300

    Refactor code
    Finish memory matcher app prototype

--- memory-matcher/app/src/main/java/gparap/apps/memory_matcher/utils/AppConstants.kt
+++ memory-matcher/app/src/main/java/gparap/apps/memory_matcher/utils/AppConstants.kt
@@ -1,10 +1,14 @@
 package gparap.apps.memory_matcher.utils
 
 object AppConstants {
+    const val CARD_HIDING_TIMEOUT   = 667L
+
     //state
     const val KEY_IS_GRID_FILLED    = "is_grid_filled"
     const val KEY_GRID_SIZE         = "grid_size"
     const val KEY_GRID_CARD_LIST    = "grid_card_list"
+    const val KEY_APP_TOTAL_MOVES   = "app_total_moves"
+    const val KEY_APP_MATCHED_PAIRS = "app_matched_pairs"
 
     //path
     const val PATH_PLANETS          = "planets"
