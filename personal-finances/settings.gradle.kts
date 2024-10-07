e1d8463941c23f2059a071dfb2f1fc6d0a41f212
gparap <gparap@protonmail.com>
2024-10-07 12:37:16 +0300

    Initial commit (personal-finances)

---
+++ personal-finances/settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    @Suppress("UnstableApiUsage")  repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My Personal Finances App"
+include(":app")
