5cb663b9ec6cee52b7e074abafc38aa40ad0718c
gparap <gparap@protonmail.com>
2024-10-14 11:28:04 +0300

    Create basic layout for adding transactions

---
+++ personal-finances/app/src/main/java/gparap/apps/personal_finances/ui/AddTransactionActivity.kt
@@ -0,0 +1,21 @@
+package gparap.apps.personal_finances.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import gparap.apps.personal_finances.R
+
+class AddTransactionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add_transaction)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layout_activity_addTransaction)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
