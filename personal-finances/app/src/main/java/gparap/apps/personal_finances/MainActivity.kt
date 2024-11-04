48b4ac46e2a43812e7a3f0a7ad88616000de6d8a
gparap <gparap@protonmail.com>
2024-11-04 12:48:21 +0200

    Update & Display the account balance

--- personal-finances/app/src/main/java/gparap/apps/personal_finances/MainActivity.kt
+++ personal-finances/app/src/main/java/gparap/apps/personal_finances/MainActivity.kt
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2024 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package gparap.apps.personal_finances
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,8 +28,14 @@ import gparap.apps.personal_finances.ui.AddTransactionActivity
 import gparap.apps.personal_finances.ui.AllTransactionsActivity
 import gparap.apps.personal_finances.ui.ExpenseTransactionsActivity
 import gparap.apps.personal_finances.ui.TopUpTransactionsActivity
+import gparap.apps.personal_finances.utils.AppConstants
+import gparap.apps.personal_finances.utils.AppConstants.SHARED_PREF_ACCOUNT_BALANCE
+import gparap.apps.personal_finances.utils.AppConstants.ZERO_ACCOUNT_BALANCE
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+    private var accountBalance: Float = ZERO_ACCOUNT_BALANCE
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,6 +44,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //initialize account balance as a shared preference, if not exists
+        val sharedPreferences = getSharedPreferences(AppConstants.APP_SHARED_PREFERENCES, MODE_PRIVATE)
+        if (!sharedPreferences.contains(SHARED_PREF_ACCOUNT_BALANCE)) {
+            val editor = sharedPreferences.edit()
+            editor.putFloat(SHARED_PREF_ACCOUNT_BALANCE, ZERO_ACCOUNT_BALANCE)
+            editor.apply()
+        }else{
+            accountBalance = sharedPreferences.getFloat(SHARED_PREF_ACCOUNT_BALANCE, ZERO_ACCOUNT_BALANCE)
+        }
+
+        //display account balance
+        findViewById<TextView>(R.id.textView_account_balance).apply {
+            text = String.format(Locale.getDefault(), accountBalance.toString())
         }
 
         //redirect to add transaction screen
--- personal-finances/app/src/main/java/gparap/apps/personal_finances/ui/AddTransactionActivity.kt
+++ personal-finances/app/src/main/java/gparap/apps/personal_finances/ui/AddTransactionActivity.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 gparap
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package gparap.apps.personal_finances.ui
 
 import android.os.Bundle
@@ -15,6 +30,7 @@ import gparap.apps.personal_finances.R
 import gparap.apps.personal_finances.data.PersonalFinancesDatabase
 import gparap.apps.personal_finances.data.TransactionModel
 import gparap.apps.personal_finances.utils.AppConstants
+import gparap.apps.personal_finances.utils.AppConstants.SHARED_PREF_ACCOUNT_BALANCE
 import gparap.apps.personal_finances.utils.TransactionType
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
@@ -66,8 +82,8 @@ class AddTransactionActivity : AppCompatActivity() {
                             resources.getString(R.string.toast_add_transaction_require_negative),
                             Toast.LENGTH_SHORT
                         ).show()
+                        return@setOnClickListener
                     }
-                    return@setOnClickListener
                 }
             }
 
@@ -83,7 +99,7 @@ class AddTransactionActivity : AppCompatActivity() {
             //create a database handler
             val roomDB = PersonalFinancesDatabase.getInstance(this)
 
-            //add test transaction to database
+            //add transaction to database
             lifecycleScope.launch {
                 progressBar.visibility = View.VISIBLE
                 try {
@@ -102,6 +118,14 @@ class AddTransactionActivity : AppCompatActivity() {
                     ).show()
                 } finally {
                     progressBar.visibility = View.INVISIBLE
+
+                    //update account balance in shared preferences
+                    val sharedPrefs = getSharedPreferences(AppConstants.APP_SHARED_PREFERENCES, MODE_PRIVATE)
+                    var accountBalance = sharedPrefs.getFloat(SHARED_PREF_ACCOUNT_BALANCE, AppConstants.ZERO_ACCOUNT_BALANCE)
+                    accountBalance += quantity.toFloat()
+                    val editor = sharedPrefs.edit()
+                    editor.putFloat(SHARED_PREF_ACCOUNT_BALANCE, accountBalance)
+                    editor.apply()
                 }
             }
         }
