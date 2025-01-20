3c85e30030700ee79df25666871044d9ed5d7186
gparap <gparap@protonmail.com>
2025-01-20 11:14:05 +0200

    Create converter unit tests for area

--- converter-unit/app/src/test/java/gparap/apps/converter_unit/converters/AreaConverterUnitTest.java
+++ converter-unit/app/src/test/java/gparap/apps/converter_unit/converters/AreaConverterUnitTest.java
@@ -15,109 +15,146 @@
  */
 package gparap.apps.converter_unit.converters;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 
 public class AreaConverterUnitTest {
     @Test
     public void isCorrect_convertSquareMeterToSquareKilometer() {
-        fail();
+        int squareMeter = 1;
+        double squareKilometerExpected = 0.000001;
+        assert squareKilometerExpected == squareMeter * ((double) 1 / 1000000);
     }
 
     @Test
     public void isCorrect_convertSquareMeterToSquareMile() {
-        fail();
+        int squareMeter = 1;
+        double squareMileExpected = 3.86102158592535E-7;
+        assert squareMileExpected == squareMeter * (1 / 2589988.11);
     }
 
     @Test
     public void isCorrect_convertSquareMeterToHectare() {
-        fail();
+        int squareMeter = 1;
+        double hectareExpected = 0.0001;
+        assert hectareExpected == squareMeter * ((double) 1 / 10000);
     }
 
     @Test
     public void isCorrect_convertSquareMeterToAcre() {
-        fail();
+        int squareMeter = 1;
+        double acreExpected = 2.471053814671653E-4;
+        assert acreExpected == squareMeter * (1 / 4046.8564224);
     }
 
     @Test
     public void isCorrect_convertSquareKilometerToSquareMeter() {
-        fail();
+        int squareKilometer = 1;
+        double squareMeterExpected = 1000000.0;
+        assert squareMeterExpected == squareKilometer * 1000000;
     }
 
     @Test
     public void isCorrect_convertSquareKilometerToSquareMile() {
-        fail();
+        int squareKilometer = 1;
+        double squareMileExpected = 0.386102158592535;
+        assert squareMileExpected == squareKilometer * (1 / 2.58998811);
     }
 
     @Test
     public void isCorrect_convertSquareKilometerToHectare() {
-        fail();
+        int squareKilometer = 1;
+        double hectareExpected = 100.0;
+        assert hectareExpected == squareKilometer * 100;
     }
 
     @Test
     public void isCorrect_convertSquareKilometerToAcre() {
-        fail();
+        int squareKilometer = 1;
+        double acreExpected = 247.1053814672;
+        assert acreExpected == squareKilometer * 247.1053814672;
     }
 
     @Test
     public void isCorrect_convertSquareMileToSquareMeter() {
-        fail();
+        int squareMile = 1;
+        double squareMeterExpected = 2589988.11;
+        assert squareMeterExpected == squareMile * 2589988.11;
     }
 
     @Test
     public void isCorrect_convertSquareMileToSquareKilometer() {
-        fail();
+        int squareMile = 1;
+        double squareKilometerExpected = 2.58998811;
+        assert squareKilometerExpected == squareMile * 2.58998811;
     }
 
     @Test
     public void isCorrect_convertSquareMileToHectare() {
-        fail();
+        int squareMile = 1;
+        double hectareExpected = 258.998811;
+        assert hectareExpected == squareMile * 258.998811;
     }
 
     @Test
     public void isCorrect_convertSquareMileToAcre() {
-        fail();
+        int squareMile = 1;
+        double acreExpected = 640.0;
+        assert acreExpected == squareMile * 640;
     }
 
     @Test
     public void isCorrect_convertHectareToSquareMeter() {
-        fail();
+        int hectare = 1;
+        double squareMeterExpected = 10000.0;
+        assert squareMeterExpected == hectare * 10000;
     }
 
     @Test
     public void isCorrect_convertHectareToSquareKilometer() {
-        fail();
+        int hectare = 1;
+        double squareKilometerExpected = 0.01;
+        assert squareKilometerExpected == hectare * 0.01;
     }
 
     @Test
     public void isCorrect_convertHectareToSquareMile() {
-        fail();
+        int hectare = 1;
+        double squareMileExpected = 0.0038610215859253504;
+        assert squareMileExpected == hectare * (1 / 258.998811);
     }
 
     @Test
     public void isCorrect_convertHectareToAcre() {
-        fail();
+        int hectare = 1;
+        double acreExpected = 2.471053814672;
+        assert acreExpected == hectare * 2.471053814672;
     }
 
     @Test
     public void isCorrect_convertAcreToSquareMeter() {
-        fail();
+        int acre = 1;
+        double squareMeterExpected = 4046.8564224;
+        assert squareMeterExpected == acre * 4046.8564224;
     }
 
     @Test
     public void isCorrect_convertAcreToSquareKilometer() {
-        fail();
+        int acre = 1;
+        double squareKilometerExpected = 0.004046856422399432;
+        assert squareKilometerExpected == acre * (1 / 247.1053814672);
     }
 
     @Test
     public void isCorrect_convertAcreToSquareMile() {
-        fail();
+        int acre = 1;
+        double squareMileExpected = 0.0015625;
+        assert squareMileExpected == acre * ((double) 1 / 640);
     }
 
     @Test
     public void isCorrect_convertAcreToHectare() {
-        fail();
+        int acre = 1;
+        double hectareExpected = 0.40468564224;
+        assert hectareExpected == acre * 0.40468564224;
     }
-
 }
