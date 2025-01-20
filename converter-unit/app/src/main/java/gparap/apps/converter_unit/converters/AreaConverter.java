a827121fa8cbc3abc77e3fb36ad02d0616556faa
gparap <gparap@protonmail.com>
2025-01-20 14:29:15 +0200

    Create unit converter for area

--- converter-unit/app/src/main/java/gparap/apps/converter_unit/converters/AreaConverter.java
+++ converter-unit/app/src/main/java/gparap/apps/converter_unit/converters/AreaConverter.java
@@ -17,30 +17,28 @@ package gparap.apps.converter_unit.converters;
 
 public class AreaConverter {
 
-    //TODO: replace stubs with actual methods
+    public double convertSquareMeterTo_SquareKilometer(double squareMeter) { return squareMeter * ((double) 1 / 1000000); }
+    public double convertSquareMeterTo_SquareMile(double squareMeter) { return squareMeter * (1 / 2589988.11); }
+    public double convertSquareMeterTo_Hectare(double squareMeter) { return squareMeter * ((double) 1 / 10000); }
+    public double convertSquareMeterTo_Acre(double squareMeter) { return squareMeter * (1 / 4046.8564224); }
 
-    public int convertSquareMeterTo_SquareKilometer() { return 0;}
-    public int convertSquareMeterTo_SquareMile() { return 0;}
-    public int convertSquareMeterTo_Hectare() { return 0;}
-    public int convertSquareMeterTo_Acre() { return 0;}
+    public double convertSquareKilometerTo_SquareMeter(double squareKilometer) { return squareKilometer * 1000000 ;}
+    public double convertSquareKilometerTo_SquareMile(double squareKilometer) { return squareKilometer * (1 / 2.58998811); }
+    public double convertSquareKilometerTo_Hectare(double squareKilometer) { return squareKilometer * 100; }
+    public double convertSquareKilometerTo_Acre(double squareKilometer) { return squareKilometer * 247.1053814672; }
 
-    public int convertSquareKilometerTo_SquareMeter() { return 0;}
-    public int convertSquareKilometerTo_SquareMile() { return 0;}
-    public int convertSquareKilometerTo_Hectare() { return 0;}
-    public int convertSquareKilometerTo_Acre() { return 0;}
+    public double convertSquareMileTo_SquareMeter(double squareMile) { return squareMile * 2589988.11; }
+    public double convertSquareMileTo_SquareKilometer(double squareMile) { return squareMile * 2.58998811; }
+    public double convertSquareMileTo_Hectare(double squareMile) { return squareMile * 258.998811; }
+    public double convertSquareMileTo_Acre(double squareMile) { return squareMile * 640; }
 
-    public int convertSquareMileTo_SquareMeter() { return 0;}
-    public int convertSquareMileTo_SquareKilometer() { return 0;}
-    public int convertSquareMileTo_Hectare() { return 0;}
-    public int convertSquareMileTo_Acre() { return 0;}
+    public double convertHectareTo_SquareMeter(double hectare) { return hectare * 10000; }
+    public double convertHectareTo_SquareKilometer(double hectare) { return hectare * 0.01; }
+    public double convertHectareTo_SquareMile(double hectare) { return hectare * (1 / 258.998811); }
+    public double convertHectareTo_Acre(double hectare) { return hectare * 2.471053814672; }
 
-    public int convertHectareTo_SquareMeter() { return 0;}
-    public int convertHectareTo_SquareKilometer() { return 0;}
-    public int convertHectareTo_SquareMile() { return 0;}
-    public int convertHectareTo_Acre() { return 0;}
-
-    public int convertAcreTo_SquareMeter() { return 0;}
-    public int convertAcreTo_SquareKilometer() { return 0;}
-    public int convertAcreTo_SquareMile() { return 0;}
-    public int convertAcreTo_Hectare() { return 0;}
+    public double convertAcreTo_SquareMeter(double acre) { return acre * 4046.8564224; }
+    public double convertAcreTo_SquareKilometer(double acre) { return acre * (1 / 247.1053814672); }
+    public double convertAcreTo_SquareMile(double acre) { return acre * ((double) 1 / 640); }
+    public double convertAcreTo_Hectare(double acre) { return acre * 0.40468564224; }
 }
--- converter-unit/app/src/test/java/gparap/apps/converter_unit/converters/AreaConverterUnitTest.java
+++ converter-unit/app/src/test/java/gparap/apps/converter_unit/converters/AreaConverterUnitTest.java
@@ -18,143 +18,145 @@ package gparap.apps.converter_unit.converters;
 import org.junit.Test;
 
 public class AreaConverterUnitTest {
+    private final AreaConverter converter = new AreaConverter();
+
     @Test
     public void isCorrect_convertSquareMeterToSquareKilometer() {
         int squareMeter = 1;
         double squareKilometerExpected = 0.000001;
-        assert squareKilometerExpected == squareMeter * ((double) 1 / 1000000);
+        assert squareKilometerExpected == converter.convertSquareMeterTo_SquareKilometer(squareMeter);
     }
 
     @Test
     public void isCorrect_convertSquareMeterToSquareMile() {
         int squareMeter = 1;
         double squareMileExpected = 3.86102158592535E-7;
-        assert squareMileExpected == squareMeter * (1 / 2589988.11);
+        assert squareMileExpected == converter.convertSquareMeterTo_SquareMile(squareMeter);
     }
 
     @Test
     public void isCorrect_convertSquareMeterToHectare() {
         int squareMeter = 1;
         double hectareExpected = 0.0001;
-        assert hectareExpected == squareMeter * ((double) 1 / 10000);
+        assert hectareExpected == converter.convertSquareMeterTo_Hectare(squareMeter);
     }
 
     @Test
     public void isCorrect_convertSquareMeterToAcre() {
         int squareMeter = 1;
         double acreExpected = 2.471053814671653E-4;
-        assert acreExpected == squareMeter * (1 / 4046.8564224);
+        assert acreExpected == converter.convertSquareMeterTo_Acre(squareMeter);
     }
 
     @Test
     public void isCorrect_convertSquareKilometerToSquareMeter() {
         int squareKilometer = 1;
         double squareMeterExpected = 1000000.0;
-        assert squareMeterExpected == squareKilometer * 1000000;
+        assert squareMeterExpected == converter.convertSquareKilometerTo_SquareMeter(squareKilometer);
     }
 
     @Test
     public void isCorrect_convertSquareKilometerToSquareMile() {
         int squareKilometer = 1;
         double squareMileExpected = 0.386102158592535;
-        assert squareMileExpected == squareKilometer * (1 / 2.58998811);
+        assert squareMileExpected == converter.convertSquareKilometerTo_SquareMile(squareKilometer);
     }
 
     @Test
     public void isCorrect_convertSquareKilometerToHectare() {
         int squareKilometer = 1;
         double hectareExpected = 100.0;
-        assert hectareExpected == squareKilometer * 100;
+        assert hectareExpected == converter.convertSquareKilometerTo_Hectare(squareKilometer);
     }
 
     @Test
     public void isCorrect_convertSquareKilometerToAcre() {
         int squareKilometer = 1;
         double acreExpected = 247.1053814672;
-        assert acreExpected == squareKilometer * 247.1053814672;
+        assert acreExpected == converter.convertSquareKilometerTo_Acre(squareKilometer);
     }
 
     @Test
     public void isCorrect_convertSquareMileToSquareMeter() {
         int squareMile = 1;
         double squareMeterExpected = 2589988.11;
-        assert squareMeterExpected == squareMile * 2589988.11;
+        assert squareMeterExpected == converter.convertSquareMileTo_SquareMeter(squareMile);
     }
 
     @Test
     public void isCorrect_convertSquareMileToSquareKilometer() {
         int squareMile = 1;
         double squareKilometerExpected = 2.58998811;
-        assert squareKilometerExpected == squareMile * 2.58998811;
+        assert squareKilometerExpected == converter.convertSquareMileTo_SquareKilometer(squareMile);
     }
 
     @Test
     public void isCorrect_convertSquareMileToHectare() {
         int squareMile = 1;
         double hectareExpected = 258.998811;
-        assert hectareExpected == squareMile * 258.998811;
+        assert hectareExpected == converter.convertSquareMileTo_Hectare(squareMile);
     }
 
     @Test
     public void isCorrect_convertSquareMileToAcre() {
         int squareMile = 1;
         double acreExpected = 640.0;
-        assert acreExpected == squareMile * 640;
+        assert acreExpected == converter.convertSquareMileTo_Acre(squareMile);
     }
 
     @Test
     public void isCorrect_convertHectareToSquareMeter() {
         int hectare = 1;
         double squareMeterExpected = 10000.0;
-        assert squareMeterExpected == hectare * 10000;
+        assert squareMeterExpected == converter.convertHectareTo_SquareMeter(hectare);
     }
 
     @Test
     public void isCorrect_convertHectareToSquareKilometer() {
         int hectare = 1;
         double squareKilometerExpected = 0.01;
-        assert squareKilometerExpected == hectare * 0.01;
+        assert squareKilometerExpected == converter.convertHectareTo_SquareKilometer(hectare);
     }
 
     @Test
     public void isCorrect_convertHectareToSquareMile() {
         int hectare = 1;
         double squareMileExpected = 0.0038610215859253504;
-        assert squareMileExpected == hectare * (1 / 258.998811);
+        assert squareMileExpected == converter.convertHectareTo_SquareMile(hectare);
     }
 
     @Test
     public void isCorrect_convertHectareToAcre() {
         int hectare = 1;
         double acreExpected = 2.471053814672;
-        assert acreExpected == hectare * 2.471053814672;
+        assert acreExpected == converter.convertHectareTo_Acre(hectare);
     }
 
     @Test
     public void isCorrect_convertAcreToSquareMeter() {
         int acre = 1;
         double squareMeterExpected = 4046.8564224;
-        assert squareMeterExpected == acre * 4046.8564224;
+        assert squareMeterExpected == converter.convertAcreTo_SquareMeter(acre);
     }
 
     @Test
     public void isCorrect_convertAcreToSquareKilometer() {
         int acre = 1;
         double squareKilometerExpected = 0.004046856422399432;
-        assert squareKilometerExpected == acre * (1 / 247.1053814672);
+        assert squareKilometerExpected == converter.convertAcreTo_SquareKilometer(acre);
     }
 
     @Test
     public void isCorrect_convertAcreToSquareMile() {
         int acre = 1;
         double squareMileExpected = 0.0015625;
-        assert squareMileExpected == acre * ((double) 1 / 640);
+        assert squareMileExpected == converter.convertAcreTo_SquareMile(acre);
     }
 
     @Test
     public void isCorrect_convertAcreToHectare() {
         int acre = 1;
         double hectareExpected = 0.40468564224;
-        assert hectareExpected == acre * 0.40468564224;
+        assert hectareExpected == converter.convertAcreTo_Hectare(acre);
     }
 }
