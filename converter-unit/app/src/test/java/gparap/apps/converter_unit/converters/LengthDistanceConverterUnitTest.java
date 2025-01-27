351cf4fb2fd8c853214e7bd3b9f5c6c3a680bc37
gparap <gparap@protonmail.com>
2025-01-27 12:01:30 +0200

    Create converter unit tests for length/distance

--- converter-unit/app/src/test/java/gparap/apps/converter_unit/converters/LengthDistanceConverterUnitTest.java
+++ converter-unit/app/src/test/java/gparap/apps/converter_unit/converters/LengthDistanceConverterUnitTest.java
@@ -15,159 +15,216 @@
  */
 package gparap.apps.converter_unit.converters;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 
 public class LengthDistanceConverterUnitTest {
     @Test
     public void isCorrect_convertMeterToKilometer() {
-        fail();
+        int meters = 1;
+        double kilometersExpected = 0.001;
+        assert kilometersExpected == meters * 0.001;
     }
 
     @Test
     public void isCorrect_convertMeterToMile() {
-        fail();
+        int meters = 1;
+        double milesExpected = 6.21371E-4;
+        assert milesExpected == meters * 0.000621371;
     }
 
     @Test
     public void isCorrect_convertMeterToYard() {
-        fail();
+        int meters = 1;
+        double yardsExpected = 1.09361;
+        assert yardsExpected == meters * 1.09361;
     }
 
     @Test
     public void isCorrect_convertMeterToFoot() {
-        fail();
+        int meters = 1;
+        double feetExpected = 3.28084;
+        assert feetExpected == meters * 3.28084;
     }
 
     @Test
     public void isCorrect_convertMeterToInch() {
-        fail();
+        int meters = 1;
+        double inchesExpected = 39.3701;
+        assert inchesExpected == meters * 39.3701;
     }
 
     @Test
     public void isCorrect_convertKilometerToMeter() {
-        fail();
+        int kilometers = 1;
+        double metersExpected = 1000;
+        assert metersExpected == kilometers * 1000;
     }
 
     @Test
     public void isCorrect_convertKilometerToMile() {
-        fail();
+        int kilometers = 1;
+        double milesExpected = 0.621371;
+        assert milesExpected == kilometers * 0.621371;
     }
 
     @Test
     public void isCorrect_convertKilometerToYard() {
-        fail();
+        int kilometers = 1;
+        double yardsExpected = 1093.61;
+        assert yardsExpected == kilometers * 1093.61;
     }
 
     @Test
     public void isCorrect_convertKilometerToFoot() {
-        fail();
+        int kilometers = 1;
+        double feetExpected = 3280.84;
+        assert feetExpected == kilometers * 3280.84;
     }
 
     @Test
     public void isCorrect_convertKilometerToInch() {
-        fail();
+        int kilometers = 1;
+        double inchesExpected = 39370.1;
+        assert inchesExpected == kilometers * 39370.1;
     }
 
     @Test
     public void isCorrect_convertMileToMeter() {
-        fail();
+        int miles = 1;
+        double metersExpected = 1609.34;
+        assert metersExpected == miles * 1609.34;
     }
 
     @Test
     public void isCorrect_convertMileToKilometer() {
-        fail();
+        int miles = 1;
+        double kilometersExpected = 1.60934;
+        assert kilometersExpected == miles * 1.60934;
     }
 
     @Test
     public void isCorrect_convertMileToYard() {
-        fail();
+        int miles = 1;
+        double yardsExpected = 1760;
+        assert yardsExpected == miles * 1760;
     }
 
     @Test
     public void isCorrect_convertMileToFoot() {
-        fail();
+        int miles = 1;
+        double feetExpected = 5280;
+        assert feetExpected == miles * 5280;
     }
 
     @Test
     public void isCorrect_convertMileToInch() {
-        fail();
+        int miles = 1;
+        double inchesExpected = 63360;
+        assert inchesExpected == miles * 63360;
     }
 
     @Test
     public void isCorrect_convertYardToMeter() {
-        fail();
+        int yards = 1;
+        double metersExpected = 0.9144;
+        assert metersExpected == yards * 0.9144;
     }
 
     @Test
     public void isCorrect_convertYardToKilometer() {
-        fail();
+        int yards = 1;
+        double kilometersExpected = 9.144E-4;
+        assert kilometersExpected == yards * 0.0009144;
     }
 
     @Test
     public void isCorrect_convertYardToMile() {
-        fail();
+        int yards = 1;
+        double milesExpected = 5.68182E-4;
+        assert milesExpected == yards * 0.000568182;
     }
 
     @Test
     public void isCorrect_convertYardToFoot() {
-        fail();
+        int yards = 1;
+        double feetExpected = 3;
+        assert feetExpected == yards * 3;
     }
 
     @Test
     public void isCorrect_convertYardToInch() {
-        fail();
+        int yards = 1;
+        double inchesExpected = 36;
+        assert inchesExpected == yards * 36;
     }
 
     @Test
     public void isCorrect_convertFootToMeter() {
-        fail();
+        int feet = 1;
+        double metersExpected = 0.3048;
+        assert metersExpected == feet * 0.3048;
     }
 
     @Test
     public void isCorrect_convertFootToKilometer() {
-        fail();
+        int feet = 1;
+        double kilometersExpected = 3.048E-4;
+        assert kilometersExpected == feet * 0.0003048;
     }
 
     @Test
     public void isCorrect_convertFootToMile() {
-        fail();
+        int feet = 1;
+        double milesExpected = 1.89394E-4;
+        assert milesExpected == feet * 0.000189394;
     }
 
     @Test
     public void isCorrect_convertFootToYard() {
-        fail();
+        int feet = 1;
+        double yardsExpected = 0.333333;
+        assert yardsExpected == feet * 0.333333;
     }
 
     @Test
     public void isCorrect_convertFootToInch() {
-        fail();
+        int feet = 1;
+        double inchesExpected = 12;
+        assert inchesExpected == feet * 12;
     }
 
     @Test
     public void isCorrect_convertInchToMeter() {
-        fail();
+        int inches = 1;
+        double metersExpected = 0.0254;
+        assert metersExpected == inches * 0.0254;
     }
 
     @Test
     public void isCorrect_convertInchToKilometer() {
-        fail();
+        int inches = 1;
+        double kilometersExpected = 2.54E-5;
+        assert kilometersExpected == inches * 0.0000254;
     }
 
     @Test
     public void isCorrect_convertInchToMile() {
-        fail();
+        int inches = 1;
+        double milesExpected = 1.57828E-5;
+        assert milesExpected == inches * 0.0000157828;
     }
 
     @Test
     public void isCorrect_convertInchToYard() {
-        fail();
+        int inches = 1;
+        double yardsExpected = 0.0277778;
+        assert yardsExpected == inches * 0.0277778;
     }
 
     @Test
     public void isCorrect_convertInchToFoot() {
-        fail();
+        int inches = 1;
+        double feetExpected = 0.0833333;
+        assert feetExpected == inches * 0.0833333;
     }
-
 }
