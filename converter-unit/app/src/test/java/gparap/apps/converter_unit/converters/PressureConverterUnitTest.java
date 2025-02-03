82189a678c3a4f515723abbd8e6ec1ebec223665
gparap <gparap@protonmail.com>
2025-02-03 09:55:21 +0200

    Create converter unit tests for pressure

--- converter-unit/app/src/test/java/gparap/apps/converter_unit/converters/PressureConverterUnitTest.java
+++ converter-unit/app/src/test/java/gparap/apps/converter_unit/converters/PressureConverterUnitTest.java
@@ -15,219 +15,301 @@
  */
 package gparap.apps.converter_unit.converters;
 
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 
 public class PressureConverterUnitTest {
+
     @Test
     public void isCorrect_convertPascalToBar() {
-        fail();
+        double pascals = 1;
+        double barsExpected = 100000;
+        assert barsExpected == 100000 * pascals;
     }
 
     @Test
     public void isCorrect_convertPascalToAtmosphere() {
-        fail();
+        double pascals = 1;
+        double atmospheresExpected = 101325;
+        assert atmospheresExpected == 101325 * pascals;
     }
 
     @Test
     public void isCorrect_convertPascalToTorr() {
-        fail();
+        double pascals = 1;
+        double torrExpected = 133.322;
+        assert torrExpected == 133.322 * pascals;
     }
 
     @Test
     public void isCorrect_convertPascalToPoundPerSquareInch() {
-        fail();
+        double pascals = 1;
+        double poundsPerSquareInchExpected = 6894.76;
+        assert poundsPerSquareInchExpected == 6894.76 * pascals;
     }
 
     @Test
     public void isCorrect_convertPascalToKilopascal() {
-        fail();
+        double pascals = 1;
+        double kilopascalsExpected = 1000;
+        assert kilopascalsExpected == 1000 * pascals;
     }
 
     @Test
     public void isCorrect_convertPascalToMillimeterOfMercury() {
-        fail();
+        double pascals = 1;
+        double millimetersOfMercuryExpected = 133.322;
+        assert millimetersOfMercuryExpected == 133.322 * pascals;
     }
 
     @Test
     public void isCorrect_convertBarToPascal() {
-        fail();
+        double bars = 1;
+        double pascalExpected = 1.0E-5;
+        assert pascalExpected == 1 * Math.pow(10, -5) * bars;
     }
 
     @Test
     public void isCorrect_convertBarToAtmosphere() {
-        fail();
+        double bars = 1;
+        double atmospheresExpected = 1.01325;
+        assert atmospheresExpected == 1.01325 * bars;
     }
 
     @Test
     public void isCorrect_convertBarToTorr() {
-        fail();
+        double bars = 1;
+        double torrExpected = 0.0013332200000000002;
+        assert torrExpected == 1.33322 * Math.pow(10, -3) * bars;
     }
 
     @Test
     public void isCorrect_convertBarToPoundPerSquareInch() {
-        fail();
+        double bars = 1;
+        double poundsPerSquareInchExpected = 0.0689476;
+        assert poundsPerSquareInchExpected == 6.89476 * Math.pow(10, -2) * bars;
     }
 
     @Test
     public void isCorrect_convertBarToKilopascal() {
-        fail();
+        double bars = 1;
+        double kilopascalsExpected = 0.01;
+        assert kilopascalsExpected == 0.01 * bars;
     }
 
     @Test
     public void isCorrect_convertBarToMillimeterOfMercury() {
-        fail();
+        double bars = 1;
+        double millimetersOfMercuryExpected = 0.0013332200000000002;
+        assert millimetersOfMercuryExpected == 1.33322 * Math.pow(10, -3) * bars;
     }
 
     @Test
     public void isCorrect_convertAtmosphereToPascal() {
-        fail();
+        double atmospheres = 1;
+        double pascalExpected = 9.869229999999999E-6;
+        assert pascalExpected == 9.86923 * Math.pow(10, -6) * atmospheres;
     }
 
     @Test
     public void isCorrect_convertAtmosphereToBar() {
-        fail();
+        double atmospheres = 1;
+        double barsExpected = 0.986923;
+        assert barsExpected == 0.986923 * atmospheres;
     }
 
     @Test
     public void isCorrect_convertAtmosphereToTorr() {
-        fail();
+        double atmospheres = 1;
+        double torrExpected = 0.00131579;
+        assert torrExpected == 1.31579 * Math.pow(10, -3) * atmospheres;
     }
 
     @Test
     public void isCorrect_convertAtmosphereToPoundPerSquareInch() {
-        fail();
+        double atmospheres = 1;
+        double poundsPerSquareInchExpected = 0.068046;
+        assert poundsPerSquareInchExpected == 6.8046 * Math.pow(10, -2) * atmospheres;
     }
 
     @Test
     public void isCorrect_convertAtmosphereToKilopascal() {
-        fail();
+        double atmospheres = 1;
+        double kilopascalsExpected = 0.00986923;
+        assert kilopascalsExpected == 9.86923 * Math.pow(10, -3) * atmospheres;
     }
 
     @Test
     public void isCorrect_convertAtmosphereToMillimeterOfMercury() {
-        fail();
+        double atmospheres = 1;
+        double millimetersOfMercuryExpected = 0.00131579;
+        assert millimetersOfMercuryExpected == 1.31579 * Math.pow(10, -3) * atmospheres;
     }
 
     @Test
     public void isCorrect_convertTorrToPascal() {
-        fail();
+        double torr = 1;
+        double pascalExpected = 0.0075006199999999995;
+        assert pascalExpected == 7.50062 * Math.pow(10, -3) * torr;
     }
 
     @Test
     public void isCorrect_convertTorrToBar() {
-        fail();
+        double torr = 1;
+        double barsExpected = 750.062;
+        assert barsExpected == 750.062 * torr;
     }
 
     @Test
     public void isCorrect_convertTorrToAtmosphere() {
-        fail();
+        double torr = 1;
+        double atmospheresExpected = 760;
+        assert atmospheresExpected == 760 * torr;
     }
 
     @Test
     public void isCorrect_convertTorrToPoundPerSquareInch() {
-        fail();
+        double torr = 1;
+        double poundsPerSquareInchExpected = 51.7149;
+        assert poundsPerSquareInchExpected == 51.7149 * torr;
     }
 
     @Test
     public void isCorrect_convertTorrToKilopascal() {
-        fail();
+        double torr = 1;
+        double kilopascalsExpected = 7.50062;
+        assert kilopascalsExpected == 7.50062 * torr;
     }
 
     @Test
     public void isCorrect_convertTorrToMillimeterOfMercury() {
-        fail();
+        double torr = 1;
+        double millimetersOfMercuryExpected = 1;
+        assert millimetersOfMercuryExpected == 1 * torr;
     }
 
     @Test
     public void isCorrect_convertPoundPerSquareInchToPascal() {
-        fail();
+        double poundsPerSquareInch = 1;
+        double pascalExpected = 1.45038E-4;
+        assert pascalExpected == 1.45038 * Math.pow(10, -4) * poundsPerSquareInch;
     }
 
     @Test
     public void isCorrect_convertPoundPerSquareInchToBar() {
-        fail();
+        double poundsPerSquareInch = 1;
+        double barsExpected = 14.5038;
+        assert barsExpected == 14.5038 * poundsPerSquareInch;
     }
 
     @Test
     public void isCorrect_convertPoundPerSquareInchToAtmosphere() {
-        fail();
+        double poundsPerSquareInch = 1;
+        double atmospheresExpected = 14.6959;
+        assert atmospheresExpected == 14.6959 * poundsPerSquareInch;
     }
 
     @Test
     public void isCorrect_convertPoundPerSquareInchToTorr() {
-        fail();
+        double poundsPerSquareInch = 1;
+        double torrExpected = 0.0193368;
+        assert torrExpected == 1.93368 * Math.pow(10, -2) * poundsPerSquareInch;
     }
 
     @Test
     public void isCorrect_convertPoundPerSquareInchToKilopascal() {
-        fail();
+        double poundsPerSquareInch = 1;
+        double kilopascalsExpected = 0.145038;
+        assert kilopascalsExpected == 0.145038 * poundsPerSquareInch;
     }
 
     @Test
     public void isCorrect_convertPoundPerSquareInchToMillimeterOfMercury() {
-        fail();
+        double poundsPerSquareInch = 1;
+        double millimetersOfMercuryExpected = 0.0193368;
+        assert millimetersOfMercuryExpected == 1.93368 * Math.pow(10, -2) * poundsPerSquareInch;
     }
 
     @Test
     public void isCorrect_convertKilopascalToPascal() {
-        fail();
+        double kilopascals = 1;
+        double pascalExpected = 0.001;
+        assert pascalExpected == 0.001 * kilopascals;
     }
 
     @Test
     public void isCorrect_convertKilopascalToBar() {
-        fail();
+        double kilopascals = 1;
+        double barsExpected = 100;
+        assert barsExpected == 100 * kilopascals;
     }
 
     @Test
     public void isCorrect_convertKilopascalToAtmosphere() {
-        fail();
+        double kilopascals = 1;
+        double atmospheresExpected = 101.325;
+        assert atmospheresExpected == 101.325 * kilopascals;
     }
 
     @Test
     public void isCorrect_convertKilopascalToTorr() {
-        fail();
+        double kilopascals = 1;
+        double torrExpected = 0.133322;
+        assert torrExpected == 0.133322 * kilopascals;
     }
 
     @Test
     public void isCorrect_convertKilopascalToPoundPerSquareInch() {
-        fail();
+        double kilopascals = 1;
+        double poundsPerSquareInchExpected = 6.89476;
+        assert poundsPerSquareInchExpected == 6.89476 * kilopascals;
     }
 
     @Test
     public void isCorrect_convertKilopascalToMillimeterOfMercury() {
-        fail();
+        double kilopascals = 1;
+        double millimetersOfMercuryExpected = 0.133322;
+        assert millimetersOfMercuryExpected == 0.133322 * kilopascals;
     }
 
     @Test
     public void isCorrect_convertMillimeterOfMercuryToPascal() {
-        fail();
+        double millimetersOfMercury = 1;
+        double pascalExpected = 0.0075006199999999995;
+        assert pascalExpected == 7.50062 * Math.pow(10, -3) * millimetersOfMercury;
     }
 
     @Test
     public void isCorrect_convertMillimeterOfMercuryToBar() {
-        fail();
+        double millimetersOfMercury = 1;
+        double barsExpected = 750.062;
+        assert barsExpected == 750.062 * millimetersOfMercury;
     }
 
     @Test
     public void isCorrect_convertMillimeterOfMercuryToAtmosphere() {
-        fail();
+        double millimetersOfMercury = 1;
+        double atmospheresExpected = 760;
+        assert atmospheresExpected == 760 * millimetersOfMercury;
     }
 
     @Test
     public void isCorrect_convertMillimeterOfMercuryToTorr() {
-        fail();
+        double millimetersOfMercury = 1;
+        double torrExpected = 1;
+        assert torrExpected == 1 * millimetersOfMercury;
     }
 
     @Test
     public void isCorrect_convertMillimeterOfMercuryToPoundPerSquareInch() {
-        fail();
+        double millimetersOfMercury = 1;
+        double poundsPerSquareInchExpected = 51.7149;
+        assert poundsPerSquareInchExpected == 51.7149 * millimetersOfMercury;
     }
 
     @Test
     public void isCorrect_convertMillimeterOfMercuryToKilopascal() {
-        fail();
+        double millimetersOfMercury = 1;
+        double kilopascalsExpected = 7.50062;
+        assert kilopascalsExpected == 7.50062 * millimetersOfMercury;
     }
-
 }
