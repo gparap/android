f56f29ece56da1048facdd8d843deaaa412bc6cb
gparap <54770777+gparap@users.noreply.github.com>
2026-06-17 13:32:40 +0300

    Update images for services market sub-category

--- classifieds/app/src/main/java/gparap/apps/classifieds/ui/market/categories/services/ServicesFragment.java
+++ classifieds/app/src/main/java/gparap/apps/classifieds/ui/market/categories/services/ServicesFragment.java
@@ -15,6 +15,8 @@
  */
 package gparap.apps.classifieds.ui.market.categories.services;
 
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,7 +29,10 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import java.util.ArrayList;
+
 import gparap.apps.classifieds.R;
+import gparap.apps.classifieds.utils.Utils;
 
 public class ServicesFragment extends Fragment {
 
@@ -47,12 +52,35 @@ public class ServicesFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        loadServicesCategoryAssets();
+
         openServicesCategory(R.id.imageButton_marketCategory_Services_Beauty, "Beauty");
         openServicesCategory(R.id.imageButton_marketCategory_Services_Cleaning, "Cleaning");
         openServicesCategory(R.id.imageButton_marketCategory_Services_Events, "Events");
         openServicesCategory(R.id.imageButton_marketCategory_Services_Other, "Other");
         openServicesCategory(R.id.imageButton_marketCategory_Services_Repair, "Repair");
         openServicesCategory(R.id.imageButton_marketCategory_Services_Tutoring, "Tutoring");
+    }
+
+    private void loadServicesCategoryAssets() {
+        String[] assets = new String[]{"beauty.jpg", "cleaning.jpg", "events.jpg", "other.jpg", "repair.jpg", "tutoring.jpg"};
+        AssetManager assetManager = requireActivity().getAssets();
+        String path = "market/categories/services";
+        ArrayList<Drawable> drawables = Utils.getInstance().getDrawablesFromAssets(assetManager, assets, path);
+
+        //Sets the drawables as the content of the ImageViews
+        ImageView beauty = ServicesFragment.this.requireView().findViewById(R.id.imageButton_marketCategory_Services_Beauty);
+        beauty.setImageDrawable(drawables.get(0));
+        ImageView cleaning = ServicesFragment.this.requireView().findViewById(R.id.imageButton_marketCategory_Services_Cleaning);
+        cleaning.setImageDrawable(drawables.get(1));
+        ImageView events = ServicesFragment.this.requireView().findViewById(R.id.imageButton_marketCategory_Services_Events);
+        events.setImageDrawable(drawables.get(2));
+        ImageView other = ServicesFragment.this.requireView().findViewById(R.id.imageButton_marketCategory_Services_Other);
+        other.setImageDrawable(drawables.get(3));
+        ImageView repair = ServicesFragment.this.requireView().findViewById(R.id.imageButton_marketCategory_Services_Repair);
+        repair.setImageDrawable(drawables.get(4));
+        ImageView tutoring = ServicesFragment.this.requireView().findViewById(R.id.imageButton_marketCategory_Services_Tutoring);
+        tutoring.setImageDrawable(drawables.get(5));
     }
 
     private void openServicesCategory(int imageResId, String subCategoryName) {
